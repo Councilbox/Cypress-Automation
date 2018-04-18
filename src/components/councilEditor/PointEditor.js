8ce433af4e98f2a72d10fa21d294401a292fa13c
Aaron Fuentes <aaron.fuentes.cocodin@gmail.com>
2018-04-18 09:09:50 +0200

    Close #52

--- src/components/councilEditor/PointEditor.js
+++ src/components/councilEditor/PointEditor.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { graphql } from 'react-apollo';
-import { AlertConfirm, SelectInput, TextInput, RichTextInput } from '../displayComponents';
+import { AlertConfirm, SelectInput, TextInput, RichTextInput, Grid, GridItem, MajorityInput } from '../displayComponents';
 import { MenuItem } from 'material-ui';
 import { updateAgenda } from '../../queries';
 import * as CBX from '../../utils/CBX';
@@ -50,6 +50,15 @@ class PointEditor extends Component {
         }
     }
 
+    updateState = (object) => {
+        this.setState({
+            data: {
+                ...this.state.data,
+                ...object
+            }
+        });
+    }
+
     checkRequiredFields() {
         return true;
     }
@@ -63,35 +72,25 @@ class PointEditor extends Component {
         
         return(
             <Fragment>
-                <div className="row"> 
+                <div className="row" style={{width: '700px'}}> 
                     <div className="col-lg-6 col-md-6 col-xs-12">
                         <TextInput
                             floatingText={translate.convene_header}
                             type="text"
                             errorText={errors.agendaSubject}
                             value={agenda.agendaSubject}
-                            onChange={(event) => {
-                                this.setState({
-                                    data: {
-                                        ...this.state.data,
-                                        agendaSubject: event.target.value
-                                    }
-                                })
-                            }}
+                            onChange={(event) => this.updateState({
+                                agendaSubject: event.target.value
+                            })}
                         />
                     </div>
                     <div className="col-lg-6 col-md-6 col-xs-12">
                         <SelectInput
                             floatingText={translate.type}
                             value={agenda.subjectType}
-                            onChange={(event, child) => {
-                                this.setState({
-                                    data: {
-                                        ...this.state.data,
-                                        subjectType: event.target.value
-                                    }
-                                }) 
-                            }}
+                            onChange={(event, child) => this.updateState({
+                                subjectType: event.target.value
+                            })}
                         >
                             {filteredTypes.map((voting) => {
                                     return <MenuItem value={voting.value} key={`voting${voting.value}`}>{translate[voting.label]}</MenuItem>
@@ -100,20 +99,52 @@ class PointEditor extends Component {
                         </SelectInput>
                     </div>
                 </div>
+                {CBX.hasVotation(agenda.subjectType) &&
+                    <Grid>
+                        <GridItem xs={6} lg={3} md={3}>
+                            <SelectInput
+                                floatingText={translate.majority_label}
+                                value={''+agenda.majorityType}
+                                errorText={errors.majorityType}
+                                onChange={(event) => this.updateState({
+                                    majorityType: +event.target.value
+                                })}
+                            >
+                                {this.props.majorityTypes.map((majority) => {
+                                        return <MenuItem value={''+majority.value} key={`majorityType_${majority.value}`}>{translate[majority.label]}</MenuItem>
+                                    })
+                                }
+                            </SelectInput>
+                        </GridItem>
+                        <GridItem xs={6} lg={3} md={3}>
+                            {CBX.majorityNeedsInput(agenda.majorityType) && (
+                                <MajorityInput
+                                    type={agenda.majorityType}
+                                    style={{marginLeft: '1em'}}
+                                    value={agenda.majority}
+                                    divider={agenda.majorityDivider}
+                                    majorityError={errors.majority}
+                                    dividerError={errors.majorityDivider}
+                                    onChange={(value) => this.updateState({
+                                        majority: +value
+                                    })}
+                                    onChangeDivider={(value) => this.updateState({
+                                        majorityDivider: +value
+                                    })}
+                                />
+                            )}
+                        </GridItem>
+                    </Grid>
+                }
 
                 <RichTextInput
                     floatingText={translate.description}
                     type="text"
                     errorText={errors.description}
                     value={agenda.description}
-                    onChange={(value) => {                     
-                        this.setState({
-                            data: {
-                                ...this.state.data,
-                                description: value
-                            }
-                        })
-                    }}
+                    onChange={(value) => this.updateState({
+                        description: value
+                    })}
                 />
             </Fragment>
         );
