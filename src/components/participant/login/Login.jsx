fdc89a7e9725468895aacbfef6fa394bd245e02b
miguelrcDEV <miguel.rodriguez@cocodin.com>
2018-05-21 18:17:46 +0200

    Working on inputs

--- src/components/participant/login/Login.jsx
+++ src/components/participant/login/Login.jsx
@@ -4,6 +4,7 @@ import withWindowOrientation from '../../../HOCs/withWindowOrientation';
 import { ButtonIcon } from '../../../displayComponents';
 import { primary, getSecondary } from '../../../styles/colors';
 import { Tooltip, IconButton } from 'material-ui';
+import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 import moment from 'moment';
 import Header from '../Header';
 
@@ -64,8 +65,21 @@ class ParticipantLogin extends Component {
     constructor(props){
         super(props);        
         this.state = {
-            urlToken: null
+            urlToken: null,
+            email: '',
+            password: ''
         }
+    }
+
+    getEmailInputValidationState(){
+        var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return (regex.test(this.state.email) ? 'success' : 'error');
+    }
+
+    handleChange(field, event) {
+        const newState = {};
+        newState[field] = event.target.value;
+        this.setState(newState);
     }
 
     render() {
@@ -74,29 +88,31 @@ class ParticipantLogin extends Component {
             <div style={styles.viewContainer}>
                 <Header/>
                 <div style={styles.mainContainer}>
-                    {(windowSize === 'xs' && windowOrientation === "landscape") ?
-                        <div style={styles.splittedLoginContainer}>
-                            <div style={styles.councilInfoContainer}>
-                                <img src={company.logo} alt="company_logo"/>
-                            </div>
-
-                            <div style={styles.loginFormContainer}>
-                            
-                            </div>
+                    <div style={(windowSize === 'xs' && windowOrientation === "landscape") ? styles.splittedLoginContainer : styles.loginContainer}>
+                        <div style={styles.councilInfoContainer}>
+                            <img src={company.logo} alt="company_logo"/>
+                            <h3>{council.name}</h3>
+                            <h5>{moment(new Date(council.dateStart)).format('LLL')}</h5>
                         </div>
-                    :
-                        <div style={styles.loginContainer}>
-                            <div style={styles.councilInfoContainer}>
-                                <img src={company.logo} alt="company_logo"/>
-                                <h3>{council.name}</h3>
-                                <h5>{moment(new Date(council.dateStart)).format('LLL')}</h5>
-                            </div>
 
-                            <div style={styles.loginFormContainer}>
-                            
-                            </div>
+                        <div style={styles.loginFormContainer}>
+                            <form>
+                                <FormGroup
+                                    controlId="formBasicText"
+                                    validationState={this.getEmailInputValidationState()}
+                                >
+                                    <ControlLabel>Email</ControlLabel>
+                                    <FormControl
+                                        type="email"
+                                        value={this.state.email}
+                                        placeholder="Enter email"
+                                        onChange={(event) => this.handleChange('email', event)}
+                                    />
+                                    <HelpBlock>Validation is based on is valid email.</HelpBlock>
+                                </FormGroup>
+                            </form>
                         </div>
-                    }
+                    </div>
                 </div>
             </div>
         );
