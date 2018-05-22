0791ca692c7ae4caf6440944a023f9cc65b49bfd
miguelrcDEV <miguel.rodriguez@cocodin.com>
2018-05-22 14:19:24 +0200

    Login UI done. Working on #78

--- src/components/participant/login/Login.jsx
+++ src/components/participant/login/Login.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import withWindowSize from '../../../HOCs/withWindowSize';
 import withWindowOrientation from '../../../HOCs/withWindowOrientation';
 import withTranslations from '../../../HOCs/withTranslations';
-import { ButtonIcon, TextInput } from '../../../displayComponents';
-import { primary, getSecondary } from '../../../styles/colors';
-import { Tooltip, IconButton, TextField } from 'material-ui';
+import { ButtonIcon, TextInput, BasicButton } from '../../../displayComponents';
+import { getPrimary, getSecondary, lightGrey } from '../../../styles/colors';
+import { Card, Tooltip } from 'material-ui';
 import { checkValidEmail } from '../../../utils/validation';
 import moment from 'moment';
 import Header from '../Header';
+import background from '../../../assets/img/signup3.jpg';
 
 const styles = {
     viewContainer: {
@@ -23,7 +24,11 @@ const styles = {
         alignItems: 'center',
         justifyContent: 'center',
         position: 'relative',
-        backgroundColor: 'yellow'
+        backgroundImage: `url(${background})`
+    },
+    cardContainer: {
+        margin: '20px',
+        padding: '20px'
     },
     loginContainer:{
         width: '100%',
@@ -32,8 +37,7 @@ const styles = {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        position: 'relative',
-        backgroundColor: 'blue'
+        position: 'relative'
     },
     splittedLoginContainer: {
         width: '100%',
@@ -42,23 +46,27 @@ const styles = {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        position: 'relative',
-        backgroundColor: 'red'
+        position: 'relative'
     },
     councilInfoContainer: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'brown'
+        padding: '15px',
+        textAlign: 'center'
     },
     loginFormContainer: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        width: '200px',
-        height: '90px',
-        backgroundColor: 'purple'
+        padding: '15px'
+    },
+    enterButtonContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '35px'
     }
 };
 
@@ -69,6 +77,7 @@ class ParticipantLogin extends Component {
             urlToken: null,
             email: props.participant.email,
             password: '',
+            showPassword: false,
             errors: {
                 email: '',
                 password: ''
@@ -76,8 +85,8 @@ class ParticipantLogin extends Component {
         }
     }
 
-    checkFieldsValidationState(){
-        const { translate } = this.props;
+    checkFieldsValidationState = () => {
+        const { translate, council } = this.props;
 
         let errors = {
             email: '',
@@ -87,7 +96,12 @@ class ParticipantLogin extends Component {
         //CHECK REQUIRED
         errors.email = (!this.state.email.length > 0) ? translate.field_required : '';
 
-        errors.password = (!this.state.password.length > 0) ? translate.field_required : '';
+        if(council.securityType === 0){
+            errors.password = '';
+        }
+        else{
+            errors.password = (!this.state.password.length > 0) ? translate.field_required : '';
+        }
 
         // CHECK VALID EMAIL
         const validEmail = checkValidEmail(this.state.email);
@@ -97,54 +111,93 @@ class ParticipantLogin extends Component {
             ...this.state,
             errors: errors
         });
+
+        return (errors.email === '' && errors.password === '');
     }
 
-    handleChange(field, event) {
+    handleChange = (field, event) => {
         const newState = {};
         newState[field] = event.target.value;
         this.setState(newState, this.checkFieldsValidationState);
     }
 
+    login = () => {
+        const isValidForm = this.checkFieldsValidationState();
+    }
+
+    handleKeyUp = (event) => {
+        if(event.nativeEvent.keyCode === 13){
+            this.login();
+        }
+    };
+
     render() {
         const { participant, council, company, windowSize, windowOrientation, translate } = this.props;
+        const { email, password, errors, showPassword } = this.state;
+        const primaryColor = getPrimary();
+        const secondaryColor = getSecondary();
         return (
             <div style={styles.viewContainer}>
                 <Header/>
                 <div style={styles.mainContainer}>
-                    <div style={(windowSize === 'xs' && windowOrientation === "landscape") ? styles.splittedLoginContainer : styles.loginContainer}>
-                        <div style={styles.councilInfoContainer}>
-                            <img src={company.logo} alt="company_logo"/>
-                            <h3>{council.name}</h3>
-                            <h5>{moment(new Date(council.dateStart)).format('LLL')}</h5>
-                        </div>
-
-                        <div style={styles.loginFormContainer}>
-                            <form>
-                                <TextInput
-                                    onKeyUp={this.handleKeyUp}
-                                    floatingText={translate.email}
-                                    type="email"
-                                    errorText={this.state.errors.email}
-                                    value={this.state.email}
-                                    onChange={(event) => this.handleChange('email', event)}
-                                    required={true}
-                                />
-                                {/* <FormGroup
-                                    controlId="formBasicText"
-                                    validationState={this.getEmailInputValidationState()}
+                    <Card style={styles.cardContainer}>
+                        <div style={(windowSize === 'xs' && windowOrientation === "landscape") ? styles.splittedLoginContainer : styles.loginContainer}>
+                            <div style={styles.councilInfoContainer}>
+                                <Tooltip 
+                                    title={council.businessName} 
+                                    placement={"top"}
+                                    enterDelay={300}
+                                    leaveDelay={300}
                                 >
-                                    <ControlLabel>Email</ControlLabel>
-                                    <FormControl
+                                    <img src={company.logo} alt="company_logo"/>
+                                </Tooltip>
+                                <h3 style={{color: secondaryColor}}>{council.name}</h3>
+                                <span>{moment(new Date(council.dateStart)).format('LLL')}</span>
+                            </div>
+
+                            <div style={styles.loginFormContainer}>
+                                <form>
+                                    <TextInput
+                                        onKeyUp={this.handleKeyUp}
+                                        floatingText={translate.email}
                                         type="email"
-                                        value={this.state.email}
-                                        placeholder="Enter email"
+                                        errorText={errors.email}
+                                        value={email}
                                         onChange={(event) => this.handleChange('email', event)}
+                                        required={true}
+                                        disabled={true}
                                     />
-                                    <HelpBlock>Validation is based on is valid email.</HelpBlock>
-                                </FormGroup> */}
-                            </form>
+
+                                    {(council.securityType !== 0) &&
+                                        <TextInput
+                                            onKeyUp={this.handleKeyUp}
+                                            floatingText={translate.login_password}
+                                            type={showPassword ? "text" : "password"}
+                                            errorText={errors.password}
+                                            value={password}
+                                            onChange={(event) => this.handleChange('password', event)}
+                                            required={true}
+                                            showPassword={showPassword}
+                                            passwordToggler={() => this.setState({showPassword: !showPassword})}
+                                        />
+                                    }
+
+
+                                    <div style={styles.enterButtonContainer}>
+                                        <BasicButton
+                                            text={translate.enter_room}
+                                            color={primaryColor}
+                                            textStyle={{color: 'white', fontWeight: '700'}}
+                                            textPosition="before"
+                                            fullWidth={true}
+                                            icon={<ButtonIcon color='white' type="directions_walk" />}
+                                            onClick={this.login}
+                                        />
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                    </div>
+                    </Card>
                 </div>
             </div>
         );
