205e3dbccef00bffd48f503440dac7293c2bc7ac
miguelrcDEV <miguel.rodriguez@cocodin.com>
2018-06-06 10:09:00 +0200

    Fix config file

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
-/* const LOCATION_URL =
+const LOCATION_URL =
 	process.env.REACT_APP_MODE === "dev"
 		? "localhost:5000/graphql"
 		: "apicbx.councilbox.com";
 
-export const API_URL = `${window.location.protocol}//${LOCATION_URL}`; */
-
-// POINT CLIENT TO VPS SERVER
-const LOCATION_URL = process.env.REACT_APP_MODE === 'dev'? 'apicbx.councilbox.com' : 'apicbx.councilbox.com';
-
-export const API_URL = `https://${LOCATION_URL}`;
+export const API_URL = `${window.location.protocol}//${LOCATION_URL}`;
