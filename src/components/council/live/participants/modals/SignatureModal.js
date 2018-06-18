f929d59f3247e136dbe809cc00b86aee94f30001
Jose Manuel Sánchez Ramas <josemanuel.sanchez@cocodin.com>
2018-06-18 10:39:06 +0200

    live participant signature

--- src/components/council/live/participants/modals/SignatureModal.js
+++ src/components/council/live/participants/modals/SignatureModal.js
@@ -1,12 +1,22 @@
 import React, { Component, Fragment } from "react";
 import { compose, graphql } from "react-apollo";
-import { liveParticipantSignature, setLiveParticipantSignature } from "../../../../../queries/liveParticipant";
-import { CustomDialog, BasicButton, LoadingSection, ReactSignature } from "../../../../../displayComponents";
+import {
+	liveParticipantSignature,
+	setLiveParticipantSignature
+} from "../../../../../queries/liveParticipant";
+import {
+	CustomDialog,
+	BasicButton,
+	LoadingSection,
+	ReactSignature,
+	ParticipantDisplay,
+	Checkbox
+} from "../../../../../displayComponents";
 import { getPrimary } from "../../../../../styles/colors";
-
+import { canBePresentWithRemoteVote } from "../../../../../utils/CBX";
+import { PARTICIPANT_STATES } from "../../../../../constants";
 
 class SignatureModal extends Component {
-
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -15,70 +25,98 @@ class SignatureModal extends Component {
 			loading: false,
 			errors: {},
 			liveParticipantSignature: {},
-			participant: {}
+			participant: {},
+			participantState: PARTICIPANT_STATES.PHYSICALLY_PRESENT
 		};
 		this.signature = null;
 	}
 
 	close = () => {
-		this.setState({ modal: false })
+		this.setState({ modal: false });
 	};
 
 	static getDerivedStateFromProps(nextProps, prevState) {
 		if (!nextProps.data.loading) {
 			if (nextProps.data.liveParticipantSignature) {
 				return {
-					liveParticipantSignature: nextProps.data.liveParticipantSignature
+					liveParticipantSignature:
+						nextProps.data.liveParticipantSignature
 				};
 			}
 		}
-		return { liveParticipantSignature: { participantId: nextProps.participant.id } };
+		return {
+			liveParticipantSignature: {
+				participantId: nextProps.participant.id
+			}
+		};
 	}
-
 
 	save = async () => {
 		let signatureData = this.signature.toDataURL();
-		let { __typename, ...liveParticipantSignature } = this.state.liveParticipantSignature;
+		let {
+			__typename,
+			...liveParticipantSignature
+		} = this.state.liveParticipantSignature;
 		liveParticipantSignature.data = signatureData;
 
 		const response = await this.props.setLiveParticipantSignature({
 			variables: {
-				signature: liveParticipantSignature
+				signature: liveParticipantSignature,
+				state: this.state.participantState
 			}
 		});
 		if (!response.errors) {
 			this.props.data.refetch();
 			this.close();
 		}
-	}
+	};
 
 	openModal = () => {
-		this.setState({ modal: true })
-	}
+		this.setState({ modal: true });
+	};
+
+	clear = () => {
+		this.signature.handleClear();
+	};
 
 	setSignature = () => {
 		let data = this.props.data;
-		if (data.liveParticipantSignature && data.liveParticipantSignature.data) {
-			this.signature.fromDataURL(data.liveParticipantSignature.data)
+		if (
+			data.liveParticipantSignature &&
+			data.liveParticipantSignature.data
+		) {
+			this.signature.fromDataURL(data.liveParticipantSignature.data);
 		}
-	}
+	};
 
 	render() {
-		const { translate, council } = this.props;
-		const { participant } = this.state;
+		const { translate, council, participant } = this.props;
+		const { participantState } = this.state;
 		const { loading } = this.props.data;
-		const primary = getPrimary()
+		const primary = getPrimary();
+		const _canBePresentWithRemoteVote = canBePresentWithRemoteVote(
+			council.statute
+		);
 
 		return (
 			<Fragment>
 				<button onClick={this.openModal}> ABRIR FIRMA</button>
 				<CustomDialog
-					title={translate.add_participant}
+					title={translate.to_sign_and_confirm}
 					requestClose={this.close}
 					open={this.state.modal}
 					onEntered={this.setSignature}
+					disableBackdropClick
 					actions={
 						<Fragment>
+							<BasicButton
+								text={translate.clean}
+								textStyle={{
+									textTransform: "none",
+									fontWeight: "700"
+								}}
+								onClick={this.clear}
+							/>
 							<BasicButton
 								text={translate.cancel}
 								textStyle={{
@@ -101,23 +139,54 @@ class SignatureModal extends Component {
 								}}
 							/>
 						</Fragment>
-					}>
+					}
+				>
 					<div style={{ width: "600px" }}>
 						<div
 							style={{
-								height: "300px",
+								height: "400px",
 								overflow: "hidden",
 								position: "relative"
 							}}
 						>
-							<div style={{ width: '50vw', minWidth: '600px' }}>
-								<ReactSignature height={298} width={598} style={{ border: 'solid 1px' }} ref={ref => this.signature = ref} />
+							<ParticipantDisplay
+								participant={participant}
+								translate={translate}
+								delegate={false}
+								council={council}
+							/>
+							{_canBePresentWithRemoteVote ? (
+								<div>
+									<Checkbox
+										label={translate.has_remote_vote}
+										value={
+											participantState ===
+											PARTICIPANT_STATES.PRESENT_WITH_REMOTE_VOTE
+										}
+										onChange={(event, isInputChecked) =>
+											this.setState({
+												participantState: isInputChecked
+													? PARTICIPANT_STATES.PRESENT_WITH_REMOTE_VOTE
+													: PARTICIPANT_STATES.PHYSICALLY_PRESENT
+											})
+										}
+									/>
+								</div>
+							) : (
+								<br />
+							)}
+							<div style={{ width: "50vw", minWidth: "600px" }}>
+								<ReactSignature
+									height={248}
+									width={598}
+									style={{ border: "solid 1px" }}
+									ref={ref => (this.signature = ref)}
+								/>
 							</div>
 						</div>
 					</div>
 				</CustomDialog>
 			</Fragment>
-
 		);
 	}
 }
@@ -126,15 +195,15 @@ export default compose(
 	graphql(liveParticipantSignature, {
 		options: props => ({
 			variables: {
-				participantId: props.participant.id,
+				participantId: props.participant.id
 			},
 			notifyOnNetworkStatusChange: true
 		})
 	}),
 	graphql(setLiveParticipantSignature, {
-		name: 'setLiveParticipantSignature',
+		name: "setLiveParticipantSignature",
 		options: {
 			errorPolicy: "all"
 		}
-	}),
+	})
 )(SignatureModal);
