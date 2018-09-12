c072a8817a6fe9cc50d84dd7b3858be54163266e
Jose Manuel Sánchez Ramas <josemanuel.sanchez@cocodin.com>
2018-09-12 12:32:34 +0200

    fix live ui

--- src/displayComponents/ParticipantRow.js
+++ src/displayComponents/ParticipantRow.js
@@ -6,12 +6,12 @@ import { Paper } from 'material-ui';
 const ParticipantRow = ({ participant, onClick, checkBox, toDelegate, council, selected, onChange }) => {
 
 	let limitReached = null;
-	if(toDelegate){
-		limitReached = delegatedVotesLimitReached(council.statute, participant.delegatedVotes.length );
+	if (toDelegate) {
+		limitReached = delegatedVotesLimitReached(council.statute, participant.delegatedVotes.length);
 	}
 
 	return (
-		<Paper style={{display: 'flex', flexDirection: 'row', width: '100%', marginLeft: '0.2em', marginRight: '0.2em', padding: '0.3em', paddingLeft: '0.8em'}}>
+		<Paper style={{ display: 'flex', flexDirection: 'row', width: '100%', marginTop: '0.2em', padding: '0.3em', paddingLeft: '0.5em', cursor: 'pointer' }}>
 			{checkBox &&
 				<Checkbox
 					value={selected}
@@ -19,13 +19,13 @@ const ParticipantRow = ({ participant, onClick, checkBox, toDelegate, council, s
 				/>
 			}
 			<div
-				onClick={toDelegate? !limitReached? onClick : () => {} : onClick}
+				onClick={toDelegate ? !limitReached ? onClick : () => { } : onClick}
 				style={{
 					width: "100%",
 					padding: "0.4em",
 					//...(toDelegate? !limitReached? {cursor: "pointer"} : {} : {cursor: "pointer"}),
 					paddingLeft: "0.8em",
-					backgroundColor: `${toDelegate? !limitReached? 'transparent' : 'gainsboro' : 'transparent'}`,
+					backgroundColor: `${toDelegate ? !limitReached ? 'transparent' : 'gainsboro' : 'transparent'}`,
 					display: "flex",
 					flexDirection: "column"
 				}}
@@ -36,11 +36,11 @@ const ParticipantRow = ({ participant, onClick, checkBox, toDelegate, council, s
 						fontSize: "0.9rem"
 					}}
 				>
-					{`${participant.name} ${participant.surname} - ${participant.dni} ${toDelegate && limitReached? 'NO SE PUEDEN DELEGAR MÁS VOTOS EN ESTE PARTICIPANTE' : ''}`}
+					{`${participant.name} ${participant.surname} - ${participant.dni} ${toDelegate && limitReached ? 'NO SE PUEDEN DELEGAR MÁS VOTOS EN ESTE PARTICIPANTE' : ''}`}
 				</div>
 				<div style={{ fontSize: "0.8rem" }}>{`${participant.position} - ${
 					participant.email
-				} - ${participant.phone}`}</div>
+					} - ${participant.phone}`}</div>
 			</div>
 		</Paper>
 	);
