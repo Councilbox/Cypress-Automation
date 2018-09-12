b19e2aa48dce977844b384abca98524c571d8e01
Aaron Fuentes <aaron.fuentes.cocodin@gmail.com>
2018-09-12 19:46:08 +0200

    FFA dashboard

---
+++ src/components/noCompany/LogoutUser.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { BasicButton, TextInput } from '../../displayComponents';
+
+class LogoutUser extends React.Component {
+
+    state = {
+        userId: ''
+    }
+
+    toggle = async () => {
+        if(this.state.userId){
+            const response = await this.props.logoutUser({
+                variables: {
+                    userId: this.state.userId
+                }
+            });
+            console.log(response);
+        }
+    }
+
+    render(){
+        return(
+            <div style={{display: 'flex'}}>
+                <BasicButton
+                    text="Logout user"
+                    onClick={this.toggle}
+                />
+                <TextInput
+                    floatingText={'User to logout'}
+                    value={this.state.userId}
+                    onChange={event => this.setState({ userId: event.target.value })}
+                />
+            </div>
+        )
+    }
+}
+
+const logoutUser = gql`
+    mutation LogoutUser($userId: String!) {
+        logoutUser(userId: $userId) {
+            success
+        }
+    }
+`;
+
+export default graphql(logoutUser, {
+    name: 'logoutUser'
+})(LogoutUser);
--- src/components/noCompany/NoCompanyDashboard.js
+++ src/components/noCompany/NoCompanyDashboard.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { darkGrey, lightGrey } from "../../styles/colors";
 import { Block, Grid, GridItem } from '../../displayComponents';
+import ToggleRecordings from './ToggleRecordings';
+import ToggleVideo from './ToggleVideo';
+import LogoutUser from './LogoutUser';
+import RefreshUser from './RefreshUser';
 
 
 const NoCompanyDashboard = ({ translate, company, user }) => {
@@ -18,70 +22,88 @@ const NoCompanyDashboard = ({ translate, company, user }) => {
 			}}
 			className="container-fluid"
 		>
-			<div className="row" style={{ width: "100%" }}>
-				<div
-					style={{
-						width: "100%",
-						height: "calc(100vh - 3em)",
-						backgroundColor: lightGrey,
-						display: "flex",
-						alignItems: "center",
-						flexDirection: "column",
-						paddingBottom: "5em"
-					}}
-				>
+			{user.email === 'aaron.fuentes.cocodin+2@gmail.com'?
+				<Grid>
+					<GridItem xs={12} md={12} lg={12}>
+						<ToggleRecordings />
+					</GridItem>
+					<GridItem xs={12} md={12} lg={12}>
+						<ToggleVideo />
+					</GridItem>
+					<GridItem xs={12} md={12} lg={12}>
+						<LogoutUser />
+					</GridItem>
+					<GridItem xs={12} md={12} lg={12}>
+						<RefreshUser />
+					</GridItem>
+				</Grid>
+			:
+				<div className="row" style={{ width: "100%" }}>
 					<div
 						style={{
-							padding: "1em",
-							paddingTop: "2em"
+							width: "100%",
+							height: "calc(100vh - 3em)",
+							backgroundColor: lightGrey,
+							display: "flex",
+							alignItems: "center",
+							flexDirection: "column",
+							paddingBottom: "5em"
 						}}
 					>
-                        {translate.we_welcome}
-					</div>
-					<div
-						style={{
-							fontWeight: "700",
-							color: darkGrey,
-							padding: "2em",
-							fontSize: "1em",
-							paddingTop: "0.5em"
-						}}
-					>
-                        {translate.no_companies_desc}
-					</div>
-                    <Grid
-                        style={{
-                            width: "90%",
-                            marginTop: "4vh"
-                        }}
-                        spacing={8}
-                    >
-                        <GridItem xs={12} md={6} lg={4}>
-                            <Block
-                                link={`/company/create`}
-                                icon="add"
-                                text={translate.companies_add}
-                            />
-                        </GridItem>
+						<div
+							style={{
+								padding: "1em",
+								paddingTop: "2em"
+							}}
+						>
+							{translate.we_welcome}
+						</div>
+						<div
+							style={{
+								fontWeight: "700",
+								color: darkGrey,
+								padding: "2em",
+								fontSize: "1em",
+								paddingTop: "0.5em"
+							}}
+						>
+							{translate.no_companies_desc}
+						</div>
+						<Grid
+							style={{
+								width: "90%",
+								marginTop: "4vh"
+							}}
+							spacing={8}
+						>
+							<GridItem xs={12} md={6} lg={4}>
+								<Block
+									link={`/company/create`}
+									icon="add"
+									text={translate.companies_add}
+								/>
+							</GridItem>
 
-                        <GridItem xs={12} md={6} lg={4}>
-                            <Block
-                                link={`/company/link`}
-                                icon="link"
-                                text={translate.companies_link}
-                            />
-                        </GridItem>
+							<GridItem xs={12} md={6} lg={4}>
+								<Block
+									link={`/company/link`}
+									icon="link"
+									text={translate.companies_link}
+								/>
+							</GridItem>
 
-                        <GridItem xs={12} md={6} lg={4}>
-                            <Block
-                                link={`/meeting/new`}
-                                icon="video_call"
-                                text={translate.start_conference}
-                            />
-                        </GridItem>
-                    </Grid>
+							<GridItem xs={12} md={6} lg={4}>
+								<Block
+									link={`/meeting/new`}
+									icon="video_call"
+									text={translate.start_conference}
+								/>
+							</GridItem>
+
+						</Grid>
+					</div>
 				</div>
-			</div>
+			}
 		</div>
 	);
 }
