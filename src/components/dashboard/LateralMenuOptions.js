5358daa34b594feed28c6cd8aa29af8004d7b3ae
anxo <anxo.rodriguez.cocodin@gmail.com>
2019-01-24 16:39:06 +0100

    flecha lateral

--- src/components/dashboard/LateralMenuOptions.js
+++ src/components/dashboard/LateralMenuOptions.js
@@ -12,54 +12,60 @@ class LateralMenuOptions extends React.Component {
 		const { translate, company } = this.props;
 		return (
 			<div className={"dropdown-container"} style={{
-				background: darkGrey,
-				width: "95px",
-				height: "100%",
-				paddingTop: "20px",
+				background: 'transparent',
+				width: "130px",
+				// height: "500px",
+				// height: "100%",
+				// paddingTop: "20px",
 				position: "fixed",
-				top: "0",
-				// top: "45px",
+				// top: "0",
+				top: "45px",
 				left: "75px",
+				// left: "75px",
 				zIndex: "1",
+				border: "none",
+				boxShadow:"none"
 			}}>
-				<div style={{
-					width: "100%",
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}>
-					<LateralOptions
-						icon={'gavel'}
-						text={translate.council_types}
-						link={`/company/${company.id}/statutes`}
-					/>
-					<LateralOptions
-						icon={'contacts'}
-						text={translate.book}
-						link={`/company/${company.id}/book`}
-					/>
-					<LateralOptions
-						link={`/company/${company.id}/censuses`}
-						icon={'person'}
-						text={translate.censuses}
-					/>
-					<LateralOptions
-						link={`/company/${company.id}/drafts`}
-						icon={'class'}
-						text={translate.drafts}
-					/>
-					<LateralOptions
-						link={`/company/${company.id}/council/new`}
-						icon={'Nueva reunion'}
-						customIcon={<img src={logo} style={{ height: '100%', width: '100%' }} alt="councilbox-logo" />}
-						text={translate.dashboard_new}
-					/>
-					<LateralOptions
-						link={`/company/${company.id}/meeting/new`}
-						icon={'video_call'}
-						text={translate.start_conference}
-					/>
+				<div style={{ background: darkGrey, height:"100%", width: '83%',left: '21px',position: 'relative' }}>
+					<div style={{
+						width: "100%",
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}>
+						<LateralOptions
+							icon={'gavel'}
+							text={translate.council_types}
+							link={`/company/${company.id}/statutes`}
+						/>
+						<LateralOptions
+							icon={'contacts'}
+							text={translate.book}
+							link={`/company/${company.id}/book`}
+						/>
+						<LateralOptions
+							link={`/company/${company.id}/censuses`}
+							icon={'person'}
+							text={translate.censuses}
+						/>
+						<LateralOptions
+							link={`/company/${company.id}/drafts`}
+							icon={'class'}
+							text={translate.drafts}
+						/>
+						<LateralOptions
+							link={`/company/${company.id}/council/new`}
+							icon={'Nueva reunion'}
+							customIcon={<img src={logo} style={{ height: '100%', width: '100%' }} alt="councilbox-logo" />}
+							text={translate.dashboard_new}
+						/>
+						<LateralOptions
+							link={`/company/${company.id}/meeting/new`}
+							icon={'video_call'}
+							text={translate.start_conference}
+						/>
+					</div>
 				</div>
 			</div>
 		);
