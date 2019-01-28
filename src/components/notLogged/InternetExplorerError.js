21be070511f4b89f1e1bf6bc4d9c00a9a8276b86
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-01-28 10:05:59 +0100

    Added IE error page

---
+++ src/components/notLogged/InternetExplorerError.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InternetExplorerError = () => {
+    return(
+        <div>
+            Error
+        </div>
+    )
+}
+
+export default InternetExplorerError;
