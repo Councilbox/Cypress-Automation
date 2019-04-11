4eebbebb5c0125153dd3e8e289a04a91cdbca6f0
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-04-11 10:39:51 +0200

    Refactor appControl without apollo

--- src/containers/AppControl.js
+++ src/containers/AppControl.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
+import { graphql, withApollo } from 'react-apollo';
 import gql from 'graphql-tag';
 import { connect } from "react-redux";
 import * as mainActions from '../actions/mainActions';
@@ -14,43 +14,78 @@ const ConfigContext = React.createContext({
 
 export { ConfigContext };
 
-const AppControl = ({ subscribeToAppControl, companies, user = {}, data, children }) => {
-
+const AppControl = ({ subscribeToAppControl, companies, user = {}, children, client }) => {
     const [config, setConfig] = React.useState(null);
 
-    React.useEffect(() => {
-        if(!!user && !!user.id){
-            subscribeToAppControl({userId: user.id});
-        }
-    }, [user.id]);
-
-    React.useEffect(() => {
-        let newConfig = {};
-        if(!data.loading){
-            for(let field of data.appConfig){
+    const getData = React.useCallback(async companyId => {
+        const response = await client.query({
+            query: appConfig,
+            variables: {
+                userId: 'u152',
+                ...(companyId? {
+                    companyId
+                } : {})
+            }
+        });
+        if(response.data){
+            let newConfig = {};
+            for(let field of response.data.appConfig){
                 newConfig[field.name] = field.active;
             }
-
             if(companies.selected || companies.selected === 0){
                 if(companies.list[companies.selected].id === 488){
-                    newConfig.blockchain = true;
+                    //newConfig.blockchain = true;
                 }
             }
             setConfig(newConfig);
         }
-    }, [data]);
+    }, [client]);
+
+    React.useEffect(() => {
+        getData();
+    }, [getData]);
+
+
+    React.useEffect(() => {
+        if(!!user && !!user.id){
+            const subscribir = async () => {
+                const response = await client.subscribe({
+                    query: appControlChange,
+                    variables: {
+                        userId: user.id
+                    }
+                });
+                response.subscribe(subscriptionData => {
+                    if(subscriptionData.data.appControlChange.command === 'logout'){
+                        store.dispatch(mainActions.logout());
+                    }
+
+                    if(subscriptionData.data.appControlChange.command === 'refresh'){
+                        window.location.reload(true);
+                    }
+
+                    if(!subscriptionData.data.appControlChange.config) return;
+                    const newConfig = {};
+                    for(let field of subscriptionData.data.appControlChange.config){
+                        newConfig[field.name] = field.active;
+                    }
+                    setConfig({
+                        ...config,
+                        ...newConfig
+                    });
+                });
+            }
+            subscribir();
+
+        }
+    }, [user.id]);
+
 
     React.useEffect(() => {
         if(companies.selected || companies.selected === 0){
-            updateCompanyConfig();
+            getData(companies.list[companies.selected].id);
         }
     }, [companies.selected]);
-
-    const updateCompanyConfig = async () => {
-        await data.refetch({
-            companyId: companies.list[companies.selected].id
-        });
-    }
 
     return(
         <ConfigContext.Provider value={config}>
@@ -90,7 +125,10 @@ const mapStateToProps = state => ({
 });
 
 
-export default graphql(appConfig, {
+export default (connect(mapStateToProps)(withApollo(AppControl)));
+
+/*
+graphql(appConfig, {
     options: props => {
         return {
             variables: {
@@ -140,4 +178,6 @@ export default graphql(appConfig, {
 		    }
 		};
 	}
-})(connect(mapStateToProps)(AppControl));
+})
+
+*/
