570189194bbea868c74c489ee2e139f4d0dff23a
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-05-10 16:55:09 +0200

    close #279

---
+++ src/components/council/live/participants/RefreshCredsSendsButton.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { BasicButton } from '../../../../displayComponents';
+import { getSecondary } from '../../../../styles/colors';
+
+const RefreshCredsSendsButton = ({ translate, council, refreshCouncilCredsSends }) => {
+    const [loading, setLoading] = React.useState(false);
+    const secondary = getSecondary();
+
+    const refreshCredSends = async () => {
+        setLoading(true);
+        await refreshCouncilCredsSends({
+            variables: {
+                councilId: council.id
+            }
+        });
+        setLoading(false);
+    }
+
+    return (
+        <div>
+            <BasicButton
+                text={translate.refresh_convened}
+                color='white'
+                loading={loading}
+                loadingColor={secondary}
+                type="flat"
+                textStyle={{color: secondary, fontWeight: '700', border: `1px solid ${secondary}`}}
+                onClick={refreshCredSends}
+            />
+        </div>
+    )
+}
+
+const refreshSends = gql`
+    mutation RefreshCouncilCredsSends($councilId: Int!){
+        updateCredentialsSends(councilId: $councilId){
+            success
+            message
+        }
+    }
+`;
+
+export default graphql(refreshSends, {
+    name: 'refreshCouncilCredsSends'
+})(RefreshCredsSendsButton);
