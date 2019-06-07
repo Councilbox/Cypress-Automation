4d0fa46590a0026f7abd1705c9e38bedb166a073
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-06-07 16:47:10 +0200

    Fix act points v3.0

--- src/components/council/live/act/ActPointStateManager.js
+++ src/components/council/live/act/ActPointStateManager.js
@@ -9,93 +9,86 @@ import ToggleAgendaButton from '../ToggleAgendaButton';
 import ToggleVotingsButton from '../ToggleVotingsButton';
 
 
-class ActPointStateManager extends React.PureComponent {
+const ActPointStateManager = ({ agenda, council, translate, refetch, ...props }) => {
+    const [sendAct, setSendAct] = React.useState(false);
+    const primary = getPrimary();
 
-    state = {
-        sendActToVote: false
-    }
-
-    render(){
-        const { agenda, council, translate, refetch } = this.props;
-        const primary = getPrimary();
-
-        if(!this.props.active){
-            return (
-                <Tooltip title={translate.warning_unclosed_agenda}>
-                    <i
-                        className="fa fa-lock"
-                        style={{
-                            color: getSecondary(),
-                            fontSize: "2em"
-                        }}
-                    />
-                </Tooltip>
-            )
-        }
-
+    if(!props.active){
         return (
-            <React.Fragment>
-                {agenda.pointState === AGENDA_STATES.INITIAL &&
-                    <div>
-                        <ToggleAgendaButton
-                            agenda={agenda}
-                            translate={translate}
-                            refetch={refetch}
-                            active={true}
-                        />
-                    </div>
-                }
-                {agenda.votingState === AGENDA_STATES.DISCUSSION &&
-                    <ToggleVotingsButton
-                        council={council}
-                        agenda={agenda}
-                        translate={translate}
-                        refetch={refetch}
-                    />
-                }
-
-                 {agenda.votingState === AGENDA_STATES.CLOSED &&
-                    <ApproveActButton
-                        council={council}
-                        agenda={agenda}
-                        translate={translate}
-                        refetch={refetch}
-                    />
-                }
-
-                {agenda.pointState === AGENDA_STATES.DISCUSSION && agenda.votingState === AGENDA_STATES.INITIAL &&
-                    <React.Fragment>
-                        <BasicButton
-                            text={translate.save_preview_act}
-                            color={'white'}
-                            textStyle={{
-                                color: primary,
-                                fontWeight: "700",
-                                fontSize: "0.9em",
-                                textTransform: "none"
-                            }}
-                            onClick={() => this.setState({
-                                sendActToVote: true
-                            })}
-                            buttonStyle={{
-                                marginRight: "1em",
-                                border: `2px solid ${primary}`
-                            }}
-                        />
-                        <SendActToVote
-                            council={council}
-                            agenda={agenda}
-                            refetch={this.props.refetch}
-                            translate={translate}
-                            show={this.state.sendActToVote}
-                            requestClose={() => this.setState({ sendActToVote: false})}
-                        />
-                    </React.Fragment>
-
-                }
-            </React.Fragment>
+            <Tooltip title={translate.warning_unclosed_agenda}>
+                <i
+                    className="fa fa-lock"
+                    style={{
+                        color: getSecondary(),
+                        fontSize: "2em"
+                    }}
+                />
+            </Tooltip>
         )
     }
+
+    return (
+        <React.Fragment>
+            {agenda.pointState === AGENDA_STATES.INITIAL &&
+                <div>
+                    <ToggleAgendaButton
+                        agenda={agenda}
+                        council={council}
+                        translate={translate}
+                        refetch={refetch}
+                        active={true}
+                    />
+                </div>
+            }
+            {agenda.votingState === AGENDA_STATES.DISCUSSION &&
+                <ToggleVotingsButton
+                    council={council}
+                    agenda={agenda}
+                    translate={translate}
+                    refetch={refetch}
+                />
+            }
+
+             {agenda.votingState === AGENDA_STATES.CLOSED &&
+                <ApproveActButton
+                    council={council}
+                    agenda={agenda}
+                    translate={translate}
+                    refetch={refetch}
+                />
+            }
+
+            {agenda.pointState === AGENDA_STATES.DISCUSSION && agenda.votingState === AGENDA_STATES.INITIAL &&
+                <React.Fragment>
+                    <BasicButton
+                        text={translate.save_preview_act}
+                        color={'white'}
+                        textStyle={{
+                            color: primary,
+                            fontWeight: "700",
+                            fontSize: "0.9em",
+                            textTransform: "none"
+                        }}
+                        onClick={() => setSendAct(true)}
+                        buttonStyle={{
+                            marginRight: "1em",
+                            border: `2px solid ${primary}`
+                        }}
+                    />
+                    <SendActToVote
+                        council={council}
+                        agenda={agenda}
+                        refetch={refetch}
+                        translate={translate}
+                        show={sendAct}
+                        requestClose={() => setSendAct(false)}
+                    />
+                </React.Fragment>
+
+            }
+        </React.Fragment>
+    )
+
 }
 
 export default ActPointStateManager;
--- src/components/council/live/act/ActPointTabs.js
+++ src/components/council/live/act/ActPointTabs.js
@@ -7,81 +7,76 @@ import ActLiveSection from '../../writing/actEditor/ActLiveSection';
 import RecountSection from '../RecountSection';
 import Comments from '../Comments';
 
-class ActPointTabs extends React.Component {
-    state = {
-        selectedTab: 0
+
+const ActPointTabs = ({ agenda, translate, council, ...props }) => {
+    const [selectedTab, setSelectedTab] = React.useState(0);
+    const votings = React.useRef(null);
+
+    const handleChange = (event, index) => {
+        setSelectedTab(index);
     }
 
-    handleChange = (event, index) => {
-        this.setState({
-            selectedTab: index
-        })
-    }
 
-
-    render(){
-        const { agenda, translate, council } = this.props;
-
-        return(
-            <div style={{height: '100%', backgroundColor: 'white'}}>
-                <Tabs
-                    value={this.state.selectedTab}
-                    indicatorColor="secondary"
-                    textColor="secondary"
-                    onChange={this.handleChange}
-                >
-                    <Tab label={translate.proposed_act} />
-                    <Tab label={translate.voting}/>
-                    {CBX.councilHasComments(council.statute) &&
-                        <Tab label={translate.act_comments}/>
-                    }
-                </Tabs>
-                <div style={{width: '100%', height: 'calc(100% - 48px)', borderTop: '1px solid gainsboro'}}>
-                    <Scrollbar>
-                        {this.state.selectedTab === 0 &&
-                            <div style={{padding: '1.5em', paddingRight: '4.5em'}}>
-                                <ActLiveSection
-                                    agenda={agenda}
-                                    translate={translate}
-                                    council={this.props.council}
-                                    refetch={this.props.refetch}
-                                    companyId={this.props.council.companyId}
-                                    data={this.props.data}
-                                />
-                            </div>
-                        }
-                        {this.state.selectedTab === 1 &&
-                            <div style={{padding: '1.5em', paddingRight: '4.5em', paddingBottom: '2em'}}>
-                                <RecountSection
-                                    agenda={agenda}
-                                    key={`agendaRecount_${agenda.id}`}
-                                    council={this.props.council}
-                                    translate={translate}
-                                    recount={this.props.recount}
-                                    refetch={this.props.refetch}
-                                    majorityTypes={this.props.majorityTypes}
-                                />
-                                <Votings
-                                    ref={votings => (this.votings = votings)}
-                                    agenda={agenda}
-                                    majorities={this.props.data.majorities}
-                                    translate={translate}
-                                />
-                            </div>
-                        }
-                        {this.state.selectedTab === 2 &&
-                            <Comments
+    return(
+        <div style={{height: '100%', backgroundColor: 'white'}}>
+            <Tabs
+                value={selectedTab}
+                indicatorColor="secondary"
+                textColor="secondary"
+                onChange={handleChange}
+            >
+                <Tab label={translate.proposed_act} />
+                <Tab label={translate.voting}/>
+                {CBX.councilHasComments(council.statute) &&
+                    <Tab label={translate.act_comments}/>
+                }
+            </Tabs>
+            <div style={{width: '100%', height: 'calc(100% - 48px)', borderTop: '1px solid gainsboro'}}>
+                <Scrollbar>
+                    {selectedTab === 0 &&
+                        <div style={{padding: '1.5em', paddingRight: '4.5em'}}>
+                            <ActLiveSection
                                 agenda={agenda}
+                                translate={translate}
+                                council={council}
+                                refetch={props.refetch}
+                                companyId={council.companyId}
+                                data={props.data}
+                            />
+                        </div>
+                    }
+                    {selectedTab === 1 &&
+                        <div style={{padding: '1.5em', paddingRight: '4.5em', paddingBottom: '2em'}}>
+                            <RecountSection
+                                agenda={agenda}
+                                key={`agendaRecount_${agenda.id}`}
                                 council={council}
                                 translate={translate}
+                                recount={props.recount}
+                                refetch={props.refetch}
+                                majorityTypes={props.majorityTypes}
                             />
+                            <Votings
+                                ref={votings}
+                                council={council}
+                                agenda={agenda}
+                                majorities={props.data.majorities}
+                                translate={translate}
+                            />
+                        </div>
+                    }
+                    {selectedTab === 2 &&
+                        <Comments
+                            agenda={agenda}
+                            council={council}
+                            translate={translate}
+                        />
 
-                        }
-                    </Scrollbar>
-                </div>
+                    }
+                </Scrollbar>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ActPointTabs;
