3a2a9337f3ad59aec65600fe73551f2de68c8070
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-07-04 11:57:55 +0200

    close #338

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ import { unregister } from './registerServiceWorker';
 import { init } from './utils/analytics';
 init();
 
+if(process.env.REACT_APP_HOTJAR){
+	(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1389522,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+}
+
 const ThemeProviderLoad = Loadable({
 	loader: () => import('./displayComponents/ThemeProvider'),
 	loading: LoadingMainApp
