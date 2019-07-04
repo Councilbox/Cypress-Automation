f93667407b8bb0bd7a3dd50acd05690b9250ecdd
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-07-04 08:46:09 +0200

    close #335

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,9 +1,18 @@
 import ReactGa from 'react-ga';
-
+import { store } from '../containers/App';
 export const init = () => {
     ReactGa.initialize(process.env.REACT_APP_GTAG_ID);
 }
 
+const filteredEmails = /councilbox|cocodin/;
+
 export const sendGAevent = args => {
+    const state = store.getState();
+    if(state.user && state.user.email){
+        const { email } = state.user;
+        if(filteredEmails.test(email)){
+            return;
+        }
+    }
     ReactGa.event(args);
 }
