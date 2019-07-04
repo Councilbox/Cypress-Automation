d427051b67be615db5313dc2d0dd59af166e0989
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-07-04 12:04:17 +0200

    Upgraded version to 3.0.3

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { init } from './utils/analytics';
 init();
 
 if(process.env.REACT_APP_HOTJAR){
-	(function(h,o,t,j,a,r){
+    (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1389522,hjsv:6};
+        h._hjSettings={hjid:1389461,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
