157172f171a045c28977e3d502b7cea012d87844
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-07-11 17:16:11 +0200

    close #357

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -33,6 +33,6 @@ export const sendGAevent = args => {
 
 export const pageView = () => {
     if(checkShouldTrack()){
-        ReactGa.pageview(window.location.pathname.replace(/\d+\//g, '').replace(/\/\d+$/g, '') + window.location.search);
+        //ReactGa.pageview(window.location.pathname.replace(/\d+\//g, '').replace(/\/\d+$/g, '') + window.location.search);
     }
 }
