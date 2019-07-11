cf32c5a2f2dcb675009a19e0cc463e2855a6d2e4
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-07-11 13:25:14 +0200

    close #360

--- src/components/dashboard/CouncilsList.js
+++ src/components/dashboard/CouncilsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Table, CloseIcon, DateWrapper, Checkbox, Grid, GridItem } from '../../displayComponents';
 import { bHistory } from '../../containers/App';
 import { TableRow, TableCell } from 'material-ui';
+import { withRouter } from 'react-router-dom';
 import TableStyles from "../../styles/table";
 import { getPrimary } from "../../styles/colors";
 import CantCreateCouncilsModal from "./CantCreateCouncilsModal";
@@ -10,219 +11,209 @@ import { trialDaysLeft } from "../../utils/CBX";
 import { moment } from "../../containers/App";
 import { isMobile } from 'react-device-detect';
 import { Card } from 'material-ui';
+import { sendGAevent } from '../../utils/analytics';
+import { useHoverRow } from '../../hooks';
 
+const CouncilsList = ({ councils, translate, openDeleteModal, company, link, selectedIds, ...props }) => {
+    const [open, setOpen] = React.useState(false);
 
-class CouncilsList extends React.Component {
-
-    state = {
-        open: false,
-        selectedIds: new Map()
+    const openCantAccessModal = () => {
+        setOpen(true);
     }
 
-    openCantAccessModal = () => {
-        this.setState({
-            open: true
-        });
+    const closeCantAccessModal = () => {
+        setOpen(false);
     }
 
-    closeCantAccessModal = () => {
-        this.setState({
-            open: false
-        })
-    }
-
-    render() {
-        const { councils, translate, openDeleteModal, company, link, selectedIds } = this.props;
-        let headers = link === '/finished' ? [
-            { selectAll: <Checkbox onChange={this.props.selectAll} value={selectedIds.size === councils.length} /> },
-            { name: translate.date_real_start },
-            { name: translate.table_councils_duration },
-            { name: translate.name },
-            { name: '' }
-        ] : [
-                { selectAll: <Checkbox onChange={this.props.selectAll} value={selectedIds.size === councils.length} /> },
-                { name: translate.date_real_start },
-                { name: translate.name },
-                { name: '' }
-            ]
-        return (
-            <Table
-                headers={headers}
-                companyID={company.id}
-            >
-                {councils.map(council => {
-                    return (
-                        <CouncilListItem
-                            council={council}
-                            company={company}
-                            select={this.props.select}
-                            selected={selectedIds.has(council.id)}
-                            showModal={this.openCantAccessModal}
-                            key={`council${council.id}`}
-                            translate={translate}
-                            disabled={company.demo === 1 && trialDaysLeft(company, moment, TRIAL_DAYS) <= 0}
-                            openDeleteModal={openDeleteModal}
-                            link={link}
-                        />
-                    );
-                })}
-                <CantCreateCouncilsModal
-                    translate={translate}
-                    open={this.state.open}
-                    requestClose={this.closeCantAccessModal}
-                />
-            </Table>
-        )
-    }
+    let headers = link === '/finished' ? [
+        { selectAll: <Checkbox onChange={props.selectAll} value={selectedIds.size === councils.length} /> },
+        { name: translate.date_real_start },
+        { name: translate.table_councils_duration },
+        { name: translate.name },
+        { name: '' }
+    ] : [
+        { selectAll: <Checkbox onChange={props.selectAll} value={selectedIds.size === councils.length} /> },
+        { name: translate.date_real_start },
+        { name: translate.name },
+        { name: '' }
+    ]
+    return (
+        <Table
+            headers={headers}
+            companyID={company.id}
+        >
+            {councils.map(council => {
+                return (
+                    <CouncilListItem
+                        council={council}
+                        company={company}
+                        select={props.select}
+                        selected={selectedIds.has(council.id)}
+                        showModal={openCantAccessModal}
+                        key={`council${council.id}`}
+                        translate={translate}
+                        disabled={company.demo === 1 && trialDaysLeft(company, moment, TRIAL_DAYS) <= 0}
+                        openDeleteModal={openDeleteModal}
+                        link={link}
+                    />
+                );
+            })}
+            <CantCreateCouncilsModal
+                translate={translate}
+                open={open}
+                requestClose={closeCantAccessModal}
+            />
+        </Table>
+    )
 }
 
-class CouncilListItem extends React.PureComponent {
 
-    state = {
-        showActions: false
-    }
+const CouncilListItem = withRouter(({ council, company, link, translate, selected, ...props }) => {
+    const [show, handlers] = useHoverRow();
+    const primary = getPrimary();
 
-    mouseEnterHandler = () => {
-        this.setState({
-            showActions: true
-        })
-    }
-
-    mouseLeaveHandler = () => {
-        this.setState({
-            showActions: false
-        })
-    }
-
-    deleteIcon = councilID => {
-        const primary = getPrimary();
-
+    const deleteIcon = councilID => {
         return (
             <CloseIcon
                 style={{ color: primary }}
                 onClick={event => {
-                    this.props.openDeleteModal(councilID);
+                    props.openDeleteModal(councilID);
                     event.stopPropagation();
                 }}
             />
         );
     }
 
+    const getSectionTranslation = type => {
+		const texts = {
+			drafts: translate.companies_draft,
+			calendar: translate.companies_calendar,
+			live: translate.companies_live,
+			act: translate.companies_writing,
+			confirmed: translate.act_book,
+			history: translate.dashboard_historical
+		}
 
-    render() {
-        const { council, company, link, translate, selected } = this.props;
-        
-        if (isMobile) {
-            return (
-                <Card
-                    style={{ marginBottom: '0.5em', padding: '0.3em', position: 'relative' }}
-                    onClick={() => {
-                        this.props.disabled ?
-                            this.props.showModal()
-                            :
-                            bHistory.push(
-                                `/company/${company.id}/council/${council.id}${link}`
-                            )
-                    }}
-                >
-                    <Grid>
-                        <GridItem xs={4} md={4} style={{ fontWeight: '700' }}>
-                            {translate.name}
-                        </GridItem>
-                        <GridItem xs={7} md={7}>
-                            {council.name || translate.dashboard_new}
-                        </GridItem>
+		return texts[type];
+	}
 
-                        {link === '/finished' ?
-                            <React.Fragment>
-                                <GridItem xs={4} md={4} style={{ fontWeight: '700' }}>
-                                    {translate.table_councils_duration}
-                                </GridItem>
-                                <GridItem xs={7} md={7}>
-                                    <DateWrapper
-                                        format="HH:mm"
-                                        date={
-                                            council.dateRealStart
-                                        }
-                                    /> {` - `}
-                                    <DateWrapper
-                                        format="HH:mm"
-                                        date={
-                                            council.dateEnd
-                                        }
-                                    />
-                                </GridItem>
-                            </React.Fragment>
-                            :
-                            <React.Fragment>
-                                <GridItem xs={4} md={4} style={{ fontWeight: '700' }}>
-                                    {translate.date_real_start}
-                                </GridItem>
-                                <GridItem xs={7} md={7}>
-                                    <DateWrapper
-                                        format="DD/MM/YYYY"
-                                        date={
-                                            council.dateStart
-                                        }
-                                    />
-                                </GridItem>
-                            </React.Fragment>
-                        }
-                    </Grid>
-                    <div style={{ position: 'absolute', top: '5px', right: '5px' }}>
-                        {this.deleteIcon(council.id)}
-                    </div>
-                </Card>
-            )
-        }
-
+    if (isMobile) {
         return (
-            <TableRow
-                hover
-                onMouseOver={this.mouseEnterHandler}
-                selected={selected}
-                onMouseLeave={this.mouseLeaveHandler}
-                style={{ ...TableStyles.ROW, backgroundColor: this.props.disabled ? 'whiteSmoke' : 'inherit' }}
+            <Card
+                style={{ marginBottom: '0.5em', padding: '0.3em', position: 'relative' }}
                 onClick={() => {
-                    this.props.disabled ?
-                        this.props.showModal()
+                    props.disabled ?
+                        props.showModal()
                         :
+                        sendGAevent({
+                            category: 'Reuniones',
+                            action: `${getSectionTranslation(props.match.params.section)} - Acceso`,
+                            label: company.businessName
+                        })
                         bHistory.push(
                             `/company/${company.id}/council/${council.id}${link}`
                         )
                 }}
             >
-                <TableCell onClick={event => event.stopPropagation()} style={{ cursor: 'auto' }}>
-                    <div style={{ width: '2em' }}>
-                        {(this.state.showActions || selected) &&
-                            <Checkbox
-                                value={selected}
-                                onChange={() =>
-                                    this.props.select(council.id)
-                                }
-                            />
-                        }
-                    </div>
-                </TableCell>
+                <Grid>
+                    <GridItem xs={4} md={4} style={{ fontWeight: '700' }}>
+                        {translate.name}
+                    </GridItem>
+                    <GridItem xs={7} md={7}>
+                        {council.name || translate.dashboard_new}
+                    </GridItem>
+
+                    {link === '/finished' ?
+                        <React.Fragment>
+                            <GridItem xs={4} md={4} style={{ fontWeight: '700' }}>
+                                {translate.table_councils_duration}
+                            </GridItem>
+                            <GridItem xs={7} md={7}>
+                                <DateWrapper
+                                    format="HH:mm"
+                                    date={
+                                        council.dateRealStart
+                                    }
+                                /> {` - `}
+                                <DateWrapper
+                                    format="HH:mm"
+                                    date={
+                                        council.dateEnd
+                                    }
+                                />
+                            </GridItem>
+                        </React.Fragment>
+                        :
+                        <React.Fragment>
+                            <GridItem xs={4} md={4} style={{ fontWeight: '700' }}>
+                                {translate.date_real_start}
+                            </GridItem>
+                            <GridItem xs={7} md={7}>
+                                <DateWrapper
+                                    format="DD/MM/YYYY"
+                                    date={
+                                        council.dateStart
+                                    }
+                                />
+                            </GridItem>
+                        </React.Fragment>
+                    }
+                </Grid>
+                <div style={{ position: 'absolute', top: '5px', right: '5px' }}>
+                    {deleteIcon(council.id)}
+                </div>
+            </Card>
+        )
+    }
+
+    return (
+        <TableRow
+            hover
+            {...handlers}
+            selected={selected}
+            style={{ ...TableStyles.ROW, backgroundColor: props.disabled ? 'whiteSmoke' : 'inherit' }}
+            onClick={() => {
+                props.disabled ?
+                    props.showModal()
+                    :
+                    sendGAevent({
+                        category: 'Reuniones',
+                        action: `${getSectionTranslation(props.match.params.section)} - Acceso`,
+                        label: company.businessName
+                    })
+                    bHistory.push(
+                        `/company/${company.id}/council/${council.id}${link}`
+                    )
+            }}
+        >
+            <TableCell onClick={event => event.stopPropagation()} style={{ cursor: 'auto' }}>
+                <div style={{ width: '2em' }}>
+                    {(show || selected) &&
+                        <Checkbox
+                            value={selected}
+                            onChange={() =>
+                                props.select(council.id)
+                            }
+                        />
+                    }
+                </div>
+            </TableCell>
+            <TableCell
+                style={TableStyles.TD}
+            >
+                <DateWrapper
+                    format="DD/MM/YYYY"
+                    date={council.dateStart}
+                />
+            </TableCell>
+            {link === '/finished' &&
                 <TableCell
-                    style={TableStyles.TD}
-                >
-                    <DateWrapper
-                        format="DD/MM/YYYY"
-                        date={
-                            council.dateStart
-                        }
-                    />
-                </TableCell>
-                {link === '/finished' && <TableCell
                     style={TableStyles.TD}
                 >
                     <div style={{ width: '15em', display: 'flex', flexDirection: 'row' }}>
                         <DateWrapper
                             format="HH:mm"
-                            date={
-                                council.dateRealStart
-                            }
+                            date={council.dateRealStart}
                         /> {` - `}
                         <DateWrapper
                             format="HH:mm"
@@ -231,30 +222,26 @@ class CouncilListItem extends React.PureComponent {
                             }
                         />
                     </div>
-                </TableCell>}
-                <TableCell
-                    style={{
-                        ...TableStyles.TD,
-                        width: "65%"
-                    }}
-                >
-                    {council.name ||
-                        translate.dashboard_new}
                 </TableCell>
-                <TableCell
-                    style={TableStyles.TD}
-                >
-                    <div style={{ width: '3em' }}>
-                        {this.state.showActions &&
-                            this.deleteIcon(
-                                council.id
-                            )
-                        }
-                    </div>
-                </TableCell>
-            </TableRow>
-        )
-    }
-}
+            }
+            <TableCell
+                style={{
+                    ...TableStyles.TD,
+                    width: "65%"
+                }}
+            >
+                {council.name || translate.dashboard_new}
+            </TableCell>
+            <TableCell
+                style={TableStyles.TD}
+            >
+                <div style={{ width: '3em' }}>
+                    {show && deleteIcon(council.id)}
+                </div>
+            </TableCell>
+        </TableRow>
+    )
+})
+
 
 export default CouncilsList;
