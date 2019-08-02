74f90da5b9d4a5254a14863518afa2cc9688a0a1
Anxo <anxo.rodriguez.cocodin@gmail.com>
2019-08-02 16:54:17 +0200

    wip

--- src/components/company/drafts/CompanyDraftForm.js
+++ src/components/company/drafts/CompanyDraftForm.js
@@ -5,126 +5,161 @@ import {
 	MajorityInput,
 	SelectInput,
 	TextInput,
-	Scrollbar
+	Scrollbar,
+	AlertConfirm,
+	DropDownMenu
 } from "../../../displayComponents";
 import RichTextInput from "../../../displayComponents/RichTextInput";
 import { MenuItem, TextField, Input, Icon, Collapse } from "material-ui";
 import * as CBX from "../../../utils/CBX";
 import { GOVERNING_BODY_TYPES } from "../../../constants";
 import TextArea from "antd/lib/input/TextArea";
+import { withStyles } from "material-ui";
+import PropTypes from "prop-types";
+import withWindowSize from "../../../HOCs/withWindowSize";
+import { Divider } from "material-ui";
+import { primary } from "../../../styles/colors";
 
 
 
 const levelColor = ['#b47fb6', '#7fa5b6', '#7f94b6'];
 
-const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatutes, draftTypes, rootStatutes, languages, votingTypes, majorityTypes, companyTypes }) => {
+const styles = {
+	'input': {
+		'&::placeholder': {
+			textOverflow: 'ellipsis !important',
+			color: '#0000005c'
+		}
+	}
+};
+
+const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatutes, draftTypes, rootStatutes, languages, votingTypes, majorityTypes, companyTypes, ...props }) => {
 	const [title, setTitle] = React.useState('');
 	const [search, setSearch] = React.useState('');
 	const [description, setDescription] = React.useState('Añadir una descripcion detallada');
 	const [newTag, setNewTag] = React.useState('Crear etiqueta nueva');
 	const [infoText, setInfoText] = React.useState('');
-	// const [title, setTitle] = React.useState(false);
-	console.log(draft)
-	console.log(errors)
-	console.log(companyStatutes)
-	console.log(draftTypes)
-	console.log(rootStatutes)
-	console.log(languages)
-	console.log(votingTypes)
-	console.log(majorityTypes)
-	console.log(companyTypes)
 
-	return (
-		<Grid spacing={16} style={{ height: "100%" }}>
-			<GridItem xs={12} lg={8} md={8} style={{ height: "100%" }}>
-				<Grid spacing={16} style={{ height: "100%" }}>
-					<GridItem xs={12} lg={12} md={12} >
-						<div style={{ fontSize: "18px" }}>Titulo</div>
-						<div>
-							<Input
-								placeholder={"Titulo"}
-								disableUnderline={true}
-								style={{
-									color: "rgba(0, 0, 0, 0.36)",
-									fontSize: '15px',
-									boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.25)',
-									border: "1px solid #d7d7d7",
-									width: "100%",
-									padding: '.5em 1.6em',
-									marginTop: "1em"
-								}}
-								value={title}
-								onChange={event => setTitle(event.target.value)}
-							>
-							</Input>
 
-						</div>
-					</GridItem>
-					<GridItem xs={12} lg={12} md={12} style={{ marginTop: " 1em" }}>
-						<div style={{ display: "flex", color: "#ffffff", fontSize: "12px", marginBottom: "0.5em " }}>
-							<EtiquetaBase
-								text={"Texto 1"}
-								color={'#b47fb6'}
-							/>
-							<EtiquetaBase
-								text={"Texto 2"}
-								color={'#7fa5b6'}
-							/>
-							<EtiquetaBase
-								text={"Texto 2"}
-								color={'#7f94b6'}
-							/>
-						</div>
-						<div style={{ display: "flex", color: "#ffffff", fontSize: "12px", marginBottom: "0.5em " }}>
-							<EtiquetaBase
-								text={"Texto 1"}
-								color={'#b47fb6'}
-							/>
-							<EtiquetaBase
-								text={"Texto 2"}
-								color={'#7fa5b6'}
-							/>
-						</div>
-					</GridItem>
-					<GridItem xs={12} lg={12} md={12}>
-						<div style={{ fontSize: "18px" }}>Descripcion</div>
-						<div>
-							<textarea
-								style={{
-									color: "rgba(0, 0, 0, 0.36)",
-									fontSize: '18px',
-									boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.25)',
-									border: "1px solid #d7d7d7",
-									width: "100%",
-									padding: '.5em 1.6em',
-									marginTop: "1em",
-									minHeight: "86px",
-									resize: 'none',
-								}}
-								value={description}
-								onChange={event => setDescription(event.target.value)}
-							/>
-						</div>
-					</GridItem>
-					<GridItem xs={12} lg={12} md={12}>
-						<div>
-							<RichTextInput
-								type="text"
-								translate={translate}
-								value={infoText}
-								onChange={value => { setInfoText(value) }}
-							/>
-						</div>
-					</GridItem>
-				</Grid>
-			</GridItem>
-			<GridItem xs={12} lg={4} md={4} style={{ height: "100%" }}>
+	// console.log(draft)
+	// console.log(errors)
+	// console.log(companyStatutes)
+	// console.log(draftTypes)
+	// console.log(rootStatutes)
+	// console.log(languages)
+	// console.log(votingTypes)
+	// console.log(majorityTypes)
+	// console.log(companyTypes)
+
+	const renderTitle = () => {
+		return (
+			<React.Fragment>
+				<div style={{ fontSize: "18px" }}>Titulo</div>
+				<div>
+					<Input
+						placeholder={"Titulo"}
+						disableUnderline={true}
+						style={{
+							color: "rgba(0, 0, 0, 0.36)",
+							fontSize: '15px',
+							boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.25)',
+							border: "1px solid #d7d7d7",
+							width: "100%",
+							padding: '.5em 1.6em',
+							marginTop: "1em"
+						}}
+						value={title}
+						onChange={event => setTitle(event.target.value)}
+						classes={{ input: props.classes.input }}
+					>
+					</Input>
+				</div>
+			</React.Fragment>
+		);
+	}
+	const renderEtiquetasSeleccionadas = () => {
+		return (
+			<React.Fragment>
+				<div style={{ display: "flex", color: "#ffffff", fontSize: "12px", marginBottom: "0.5em " }}>
+					<EtiquetaBase
+						text={"Texto 1"}
+						color={'#b47fb6'}
+					/>
+					<EtiquetaBase
+						text={"Texto 2"}
+						color={'#7fa5b6'}
+					/>
+					<EtiquetaBase
+						text={"Texto 2"}
+						color={'#7f94b6'}
+					/>
+				</div>
+				<div style={{ display: "flex", color: "#ffffff", fontSize: "12px", marginBottom: "0.5em " }}>
+					<EtiquetaBase
+						text={"Texto 1"}
+						color={'#b47fb6'}
+					/>
+					<EtiquetaBase
+						text={"Texto 2"}
+						color={'#7fa5b6'}
+					/>
+				</div>
+			</React.Fragment>
+		);
+	}
+
+	const renderDescripcion = () => {
+		return (
+			<React.Fragment>
+				<div style={{ fontSize: "18px" }}>Descripcion</div>
+				<div>
+					<textarea
+						style={{
+							color: "rgba(0, 0, 0, 0.36)",
+							fontSize: '18px',
+							boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.25)',
+							border: "1px solid #d7d7d7",
+							width: "100%",
+							padding: '.5em 1.6em',
+							marginTop: "1em",
+							minHeight: "86px",
+							resize: 'none',
+						}}
+						value={description}
+						onChange={event => setDescription(event.target.value)}
+					/>
+				</div>
+			</React.Fragment>
+		);
+	}
+
+	const renderRichEditor = () => {
+		return (
+			<React.Fragment>
+				<div>
+					<RichTextInput
+						type="text"
+						translate={translate}
+						value={infoText}
+						onChange={value => { setInfoText(value) }}
+					/>
+				</div>
+			</React.Fragment>
+		);
+	}
+
+	const renderSelectorEtiquetas = () => {
+		return (
+			<React.Fragment>
 				<div style={{ fontSize: "18px", display: "flex" }}>
 					<div style={{ marginRight: "0.6em" }}>Etiquetas</div>
 					<div>
 						<i className="material-icons" style={{ transform: 'scaleX(-1)', fontSize: "20px" }}>
 							local_offer
-						</i>
+							</i>
+					</div>
+					<div onClick={() => setOpenClonar(true)}>
+						ModalClonar
 					</div>
 				</div>
 				<div style={{ boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.25)', border: 'solid 1px #d7d7d7', marginTop: "1em", }}>
@@ -136,6 +171,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 								type="text"
 								value={search}
 								styleInInput={{ fontSize: "12px", color: "rgba(0, 0, 0, 0.36)" }}
+								classes={{ input: props.classes.input }}
 								onChange={event => {
 									setSearch(event.target.value);
 								}}
@@ -177,9 +213,256 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 						</div>
 					</div>
 				</div>
-			</GridItem>
-		</Grid>
-	);
+
+				<AlertConfirm
+					requestClose={() => setOpenClonar(false)}
+					open={openClonar}
+					// acceptAction={this.updateAttachment}
+					buttonAccept={translate.accept}
+					buttonCancel={translate.cancel}
+					bodyText={renderModalClonar()}
+					title={"Cargar Plantilla"}
+				/>
+
+			</React.Fragment>
+		);
+	}
+
+
+	const [openClonar, setOpenClonar] = React.useState(false);
+
+	const renderModalClonar = () => {
+		return (
+			<div>
+				<div style={{ display: "flex", justifyContent: "space-between" }}>
+					<div style={{ display: "flex", }}>
+						<div style={{ marginRight: "1em" }}>
+							<SelectInput
+								value={1}
+							>
+								<MenuItem value={1} key={1}>asdasdasd</MenuItem>
+							</SelectInput>
+						</div>
+						<div style={{ marginRight: "1em" }}>
+							<SelectInput
+								value={1}
+							>
+								<MenuItem value={1} key={1}>asdasdasd</MenuItem>
+							</SelectInput>
+						</div>
+						<div style={{ display: "flex", alignItems: "center", marginRight: "1em" }}>
+							<DropDownMenu
+								color={primary}
+								// textStyle={{height: '100%', minWidth: "15px" }}
+								loading={false}
+								styleBody={{height:"300px"}}
+								Component={() =>
+									<MenuItem
+										style={{
+											height: '100%',
+											border: " solid 1px #353434",
+											borderRadius: '3px',
+											width: '100%',
+											margin: 0,
+											padding: 0,
+											display: 'flex',
+											alignItems: 'center',
+											justifyContent: 'center',
+											marginTop: '14px'
+										}}
+									>
+										<i className="material-icons" style={{ transform: 'scaleX(-1)', fontSize: "20px" }}>
+											local_offer
+										</i>
+										Etiquetas
+
+									</MenuItem>
+								}
+								text={translate.add_agenda_point}
+								textStyle={"ETIQKETA"}
+								// anchorOrigin={{
+								// 	vertical: 'bottom',
+								// 	horizontal: 'left',
+								// }}
+								items={
+									<div style={{height:"300px", border:"solid 1px #353434", borderRadius:"3px"}}>
+										<MenuItem >
+											<div
+												style={{
+													width: "100%",
+													display: "flex",
+													flexDirection: "row",
+													justifyContent: "space-between"
+												}}
+											>
+												<TextInput
+													placeholder={"Busca una etiqueta"}
+													adornment={<Icon>search</Icon>}
+													type="text"
+													value={search}
+													styleInInput={{ fontSize: "12px", color: "rgba(0, 0, 0, 0.54)", background: "#f0f3f6" }}
+													classes={{ input: props.classes.input }}
+												// onChange={event => {
+												// 	setSearch(event.target.value);
+												// }}
+												/>
+											</div>
+										</MenuItem>
+										<Divider />
+										<div
+											style={{
+												width: "100%",
+												display: "flex",
+												flexDirection: "row",
+												justifyContent: "space-between",
+												margin:"1em"
+											}}
+										>
+											<Grid>
+												<GridItem xs={4} lg={4} md={4}>
+													<div style={{ fontWeight: "700" }}>
+														Titulo
+													</div>
+													<div style={{ color: levelColor[0] }}>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+
+													</div>
+												</GridItem>
+												<GridItem xs={4} lg={4} md={4}>
+													<div style={{ fontWeight: "700" }}>
+														Titulo
+													</div>
+													<div style={{ color: levelColor[1] }}>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+
+													</div>
+												</GridItem>
+												<GridItem xs={4} lg={4} md={4}>
+													<div style={{ fontWeight: "700" }}>
+														Titulo
+													</div>
+													<div style={{ color: levelColor[2] }}>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+														<div>asdasdasdasdas</div>
+
+													</div>
+												</GridItem>
+												<GridItem xs={12} lg={12} md={12}>
+													<div style={{display: 'flex' }}>
+														<div style={{marginRight:"1em"}}>asdasdasdasdas</div>
+														<div style={{marginRight:"1em"}}>asdasdasdasdas</div>
+														<div style={{marginRight:"1em"}}>asdasdasdasdas</div>
+														<div style={{marginRight:"1em"}}>asdasdasdasdas</div>
+														<div style={{marginRight:"1em"}}>asdasdasdasdas</div>
+
+													</div>
+												</GridItem>
+											
+											</Grid>
+										</div>
+									</div>
+								}
+							/>
+
+						</div>
+					</div>
+					<div>
+						<TextInput
+							placeholder={"Busca una etiqueta"}
+							adornment={<Icon>search</Icon>}
+							type="text"
+							value={search}
+							styleInInput={{ fontSize: "12px", color: "rgba(0, 0, 0, 0.54)", background: "#f0f3f6" }}
+							classes={{ input: props.classes.input }}
+						// onChange={event => {
+						// 	setSearch(event.target.value);
+						// }}
+						/>
+					</div>
+				</div>
+				<div style={{ marginTop: "2em", borderTop: "2px solid  #979797", }}>
+					<Grid style={{ width: "95%", margin: "0 auto", marginTop: "1em" }}>
+						<GridItem xs={5} lg={5} md={5} style={{ boxShadow: ' 0 2px 4px 0 rgba(0, 0, 0, 0.5)', padding: "0.5em" }}>
+							<div style={{ color: "#000000", textAlign: "center" }}>ASDasdas</div>
+						</GridItem>
+						<GridItem xs={2} lg={2} md={2} >
+						</GridItem>
+						<GridItem xs={5} lg={5} md={5} style={{ boxShadow: ' 0 2px 4px 0 rgba(0, 0, 0, 0.5)', padding: "0.5em" }}>
+							<div style={{ color: "#000000", textAlign: "center" }}>ASDasdas</div>
+						</GridItem>
+
+					</Grid>
+				</div>
+			</div>
+		);
+	}
+
+
+	if (props.innerWidth > 960) {
+		return (
+			<Scrollbar>
+				<Grid spacing={16} style={{ height: "100%", width: "100%" }}>
+					<GridItem xs={12} lg={8} md={8} style={{ height: "100%" }}>
+						<Grid spacing={16} style={{ height: "100%" }}>
+							<GridItem xs={12} lg={12} md={12} >
+								{renderTitle()}
+							</GridItem>
+							<GridItem xs={12} lg={12} md={12} style={{ marginTop: " 1em" }}>
+								{renderEtiquetasSeleccionadas()}
+							</GridItem>
+							<GridItem xs={12} lg={12} md={12}>
+								{renderDescripcion()}
+							</GridItem>
+							<GridItem xs={12} lg={12} md={12}>
+								{renderRichEditor()}
+							</GridItem>
+						</Grid>
+					</GridItem>
+					<GridItem xs={12} lg={4} md={4} style={{ height: "100%" }}>
+						{renderSelectorEtiquetas()}
+					</GridItem>
+				</Grid>
+			</Scrollbar >
+		);
+	} else {
+		return (
+			<Scrollbar>
+				<Grid spacing={16} style={{ height: "100%", width: "100%" }}>
+					<GridItem xs={12} lg={12} md={12} style={{ height: "100%" }}>
+						<Grid spacing={16} style={{ height: "100%" }}>
+							<GridItem xs={12} lg={8} md={8} >
+								{renderTitle()}
+							</GridItem>
+							<GridItem xs={12} lg={8} md={8} style={{ marginTop: " 1em" }}>
+								{renderSelectorEtiquetas()}
+							</GridItem>
+							<GridItem xs={12} lg={8} md={8} style={{ marginTop: " 1em" }}>
+								{renderEtiquetasSeleccionadas()}
+							</GridItem>
+							<GridItem xs={12} lg={8} md={8}>
+								{renderDescripcion()}
+							</GridItem>
+							<GridItem xs={12} lg={8} md={8}>
+								{renderRichEditor()}
+							</GridItem>
+						</Grid>
+					</GridItem>
+				</Grid>
+			</Scrollbar>
+		);
+	}
+
 }
 
 
@@ -226,6 +509,34 @@ const ContenedorEtiquetas = ({ stylesContent, color, last }) => {
 							text={"Convocatoria"}
 							color={color}
 							moreEtiquetas={true}
+							etiquetas={
+								<React.Fragment>
+									<Etiqueta
+										text={"Cabecera"}
+										color={color}
+									/>
+									<Etiqueta
+										text={"Votacion Nominal"}
+										color={color}
+										width={'60%'}
+										moreEtiquetas={true}
+										etiquetas={
+											<React.Fragment>
+												<Etiqueta
+													text={"Cabecera3"}
+													color={color}
+												/>
+												<Etiqueta
+													text={"Votacion Nominal4"}
+													color={color}
+													width={'60%'}
+													moreEtiquetas={true}
+												/>
+											</React.Fragment>
+										}
+									/>
+								</React.Fragment>
+							}
 						/>
 
 					</div>
@@ -240,24 +551,31 @@ const ContenedorEtiquetas = ({ stylesContent, color, last }) => {
 }
 
 
-const Etiqueta = ({ text, color, moreEtiquetas }) => {
+const Etiqueta = ({ text, color, moreEtiquetas, width, etiquetas }) => {
 	const [open, setOpen] = React.useState(false);
+	const [openTimeOut, setOpenTimeOut] = React.useState(false);
+
+	const toggle = () => {
+		let time = open ? 200 : 0;
+		setTimeout(() => setOpenTimeOut(!open), time);
+		setOpen(!open)
+	}
 
 	const styles = {
 		borderRadius: '14px',
 		border: `solid 1px ${color}`,
 		padding: "4px 0.8em",
-		display: open ? "block" : "inline-block",
+		display: openTimeOut ? width ? "inline-block" : "block" : "inline-block",
 		marginRight: "0.5em",
-
 		marginTop: "0.25em",
 		marginBottom: "0.25em",
+		width: open && width
 	}
 	if (moreEtiquetas) {
 		return (
 			<div style={{ ...styles }} >
 				<div style={{}}>
-					<div style={{ display: "flex", justifyContent: open && "space-between", cursor: "pointer", }} onClick={() => setOpen(!open)} >
+					<div style={{ display: "flex", justifyContent: open && "space-between", cursor: "pointer", }} onClick={toggle} >
 						<div>{text}</div>
 						<div style={{ marginTop: '-5px', height: "5px" }}>
 							{open ?
@@ -274,10 +592,17 @@ const Etiqueta = ({ text, color, moreEtiquetas }) => {
 					{moreEtiquetas &&
 						<Collapse in={open} timeout="auto" unmountOnExit >
 							<div>
-								<Etiqueta
+								{etiquetas}
+								{/* <Etiqueta
 									text={"Cabecera"}
 									color={color}
-								/>
+								/>*/}
+								{/* <Etiqueta
+									text={"Votacion Nominal"}
+									color={color}
+									width={'60%'}
+									moreEtiquetas={true}
+								/>  */}
 							</div>
 						</Collapse>
 					}
@@ -321,7 +646,11 @@ const EtiquetaBase = ({ text, color }) => {
 }
 
 
-export default CompanyDraftForm;
+CompanyDraftForm.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(withWindowSize(CompanyDraftForm));
 
 
 
--- src/displayComponents/SelectInput.js
+++ src/displayComponents/SelectInput.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Select } from "material-ui";
 import { InputLabel } from "material-ui/Input";
 import { FormControl } from "material-ui/Form";
+import { withStyles } from "material-ui";
+import PropTypes from "prop-types";
+
+
+
+
+const styles = {
+	select: {
+		color: '#0000005c'
+	}
+};
 
 const SelectInput = ({
 	floatingText,
@@ -13,37 +24,48 @@ const SelectInput = ({
 	required,
 	disabled,
 	noLabel,
-	autoWidth
-}) => (
-	<FormControl
-		style={{
-			width: "100%",
-			marginTop: "0",
-			marginBottom: "8px"
-		}}
-	>
-		{!noLabel && (
-			<InputLabel htmlFor={id}>{`${!!floatingText ? floatingText : ""}${
-				required ? "*" : ""
-			}`}</InputLabel>
-		)}
-		<Select
-			inputProps={{
-				name: floatingText,
-				id: id,
-				style: {
-					fontSize: '12px !important'
-				}
+	autoWidth,
+	...props
+}) => {
+	
+	return (
+		<FormControl
+			style={{
+				width: "100%",
+				marginTop: "0",
+				marginBottom: "8px"
 			}}
-			autoWidth={autoWidth}
-			disabled={!!disabled}
-			value={value}
-			onChange={onChange}
-			error={!!errorText}
 		>
-			{children}
-		</Select>
-	</FormControl>
-);
+			{!noLabel && (
+				<InputLabel htmlFor={id}>{`${!!floatingText ? floatingText : ""}${
+					required ? "*" : ""
+					}`}</InputLabel>
+			)}
+			<Select
+				inputProps={{
+					name: floatingText,
+					id: id,
+					style: {
+						fontSize: '12px !important'
+					}
+				}}
+				// classes={{ select: props.classes.select }}
+				autoWidth={autoWidth}
+				disabled={!!disabled}
+				value={value}
+				onChange={onChange}
+				error={!!errorText}
+				// color={'red'}
+			>
+				{children}
+			</Select>
+		</FormControl>
+	);
+}
 
-export default SelectInput;
+
+SelectInput.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SelectInput);
