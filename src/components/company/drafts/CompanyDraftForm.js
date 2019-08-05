75f6b0c55ba105487deb0c71192d370a6391894b
Aaron Fuentes <aaron.fuentes@cocodin.com>
2019-08-05 17:22:15 +0200

    Add basic client functionality

--- src/components/company/drafts/CompanyDraftForm.js
+++ src/components/company/drafts/CompanyDraftForm.js
@@ -17,7 +17,7 @@ import TextArea from "antd/lib/input/TextArea";
 import { withStyles } from "material-ui";
 import PropTypes from "prop-types";
 import withWindowSize from "../../../HOCs/withWindowSize";
-import { Divider } from "material-ui";
+import { Divider, Tooltip } from "material-ui";
 import { primary } from "../../../styles/colors";
 
 
@@ -36,9 +36,10 @@ const styles = {
 const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatutes, draftTypes, rootStatutes, languages, votingTypes, majorityTypes, companyTypes, ...props }) => {
 	const [title, setTitle] = React.useState('');
 	const [search, setSearch] = React.useState('');
-	const [description, setDescription] = React.useState('Añadir una descripcion detallada');
-	const [newTag, setNewTag] = React.useState('Crear etiqueta nueva');
+	const [description, setDescription] = React.useState('');
+	const [newTag, setNewTag] = React.useState('');//TRADUCCION
 	const [infoText, setInfoText] = React.useState('');
+	const [testTags, setTestTags] = React.useState({});
 
 
 	// console.log(draft)
@@ -51,13 +52,39 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 	// console.log(majorityTypes)
 	// console.log(companyTypes)
 
+	const removeTag = tag => {
+		delete testTags[tag];
+		setTestTags({...testTags});
+	}
+
+	const formatTagLabel = tag => {
+		return tag.segments?
+			`${tag.segments.reduce((acc, curr) => {
+				if(curr !== tag.label) return acc + (translate[curr] || curr) + '. '
+				return acc;
+			}, '')}${tag.type === 99? tag.label : translate[tag.label] || tag.label}`
+		:
+			tag.type !== 99? translate[tag.label] || tag.label : tag.label
+	}
+
+	const addTag = tag => {
+		setTestTags({
+			...testTags,
+			[formatTagLabel(tag)]: {
+				segments: tag.segments,
+				type: tag.type,
+				active: true
+			}
+		});
+	}
+
 	const renderTitle = () => {
 		return (
 			<React.Fragment>
-				<div style={{ fontSize: "18px" }}>Titulo</div>
+				<div style={{ fontSize: "18px" }}>{translate.title}</div>
 				<div>
 					<Input
-						placeholder={"Titulo"}
+						placeholder={translate.title}
 						disableUnderline={true}
 						style={{
 							color: "rgba(0, 0, 0, 0.36)",
@@ -77,41 +104,57 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 			</React.Fragment>
 		);
 	}
+
 	const renderEtiquetasSeleccionadas = () => {
+		const TagColumn = props => {
+			return(
+				<div style={{
+					display: "flex",
+					color: "#ffffff",
+					fontSize: "12px",
+					marginBottom: "0.5em ",
+					flexDirection: 'column'
+				}}>
+					{props.children}
+				</div>
+			)
+		}
+
+		const buildTagColumns = tags => {
+			const columns = {};
+			Object.keys(tags).forEach(key => {
+				const tag = tags[key];
+				columns[tag.type] = columns[tag.type]? [...columns[tag.type], key] : [key]
+			});
+
+			return columns;
+
+		}
+
+		const columns = buildTagColumns(testTags);
+
 		return (
-			<React.Fragment>
-				<div style={{ display: "flex", color: "#ffffff", fontSize: "12px", marginBottom: "0.5em " }}>
-					<EtiquetaBase
-						text={"Texto 1"}
-						color={'#b47fb6'}
-					/>
-					<EtiquetaBase
-						text={"Texto 2"}
-						color={'#7fa5b6'}
-					/>
-					<EtiquetaBase
-						text={"Texto 2"}
-						color={'#7f94b6'}
-					/>
-				</div>
-				<div style={{ display: "flex", color: "#ffffff", fontSize: "12px", marginBottom: "0.5em " }}>
-					<EtiquetaBase
-						text={"Texto 1"}
-						color={'#b47fb6'}
-					/>
-					<EtiquetaBase
-						text={"Texto 2"}
-						color={'#7fa5b6'}
-					/>
-				</div>
-			</React.Fragment>
+			<div style={{display: 'flex'}}>
+				{Object.keys(columns).map(key => (
+					<TagColumn key={`column_${key}`}>
+						{columns[key].map(tag => (
+							<EtiquetaBase
+								key={`tag_${tag}`}
+								text={tag}
+								color={getTagColor(key)}
+								action={() => removeTag(tag)}
+							/>
+						))}
+					</TagColumn>
+				))}
+			</div>
 		);
 	}
 
-	const renderDescripcion = () => {
+	const renderDescription = () => {
 		return (
 			<React.Fragment>
-				<div style={{ fontSize: "18px" }}>Descripcion</div>
+				<div style={{ fontSize: "18px" }}>{translate.description}</div>
 				<div>
 					<textarea
 						style={{
@@ -125,12 +168,23 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 							minHeight: "86px",
 							resize: 'none',
 						}}
+						placeholder={'Añadir una descripcion detallada' /*TRADUCCION*/}
 						value={description}
 						onChange={event => setDescription(event.target.value)}
 					/>
 				</div>
 			</React.Fragment>
 		);
+	}
+
+	const handleEnter = event => {
+		if(event.keyCode === 13){
+			addTag({
+				label: newTag,
+				type: 99
+			});
+			setNewTag('');
+		}
 	}
 
 	const renderRichEditor = () => {
@@ -166,7 +220,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 					<div style={{ paddingLeft: "1em", paddingRight: "1em" }}>
 						<div style={{ marginBottom: "1em" }}>
 							<TextInput
-								placeholder={"Busca una etiqueta"}
+								placeholder={"Busca una etiqueta" /*TRADUCCION*/}
 								adornment={<Icon>search</Icon>}
 								type="text"
 								value={search}
@@ -177,34 +231,107 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 								}}
 							/>
 						</div>
-						<div style={{}}	>
-							<ContenedorEtiquetas
-								color={'#b47fb6'}
-								stylesContent={{
-									border: '1px solid #c196c3',
-									color: '#b47fb6',
-								}}
-							/>
+						<div style={{}}>
+							{!!companyStatutes &&
+								<ContenedorEtiquetas
+									color={'#b47fb6'}
+									addTag={addTag}
+									title={translate.council_type}
+									stylesContent={{
+										border: '1px solid #c196c3',
+										color: '#b47fb6',
+									}}
+									tags={companyStatutes.filter(statute => !testTags[statute.title]).map(statute => (
+										{
+											label: statute.title,
+											translation: translate[statute.title],
+											type: 0
+										}
+									))}
+								/>
+							}
 							<ContenedorEtiquetas
 								color={'#7fa5b6'}
+								addTag={addTag}
+								title={'Órganos de gobierno'/*TRADUCCION*/}
 								stylesContent={{
 									border: '1px solid #7fa5b6',
 									color: '#7fa5b6',
 								}}
+								tags={Object.keys(GOVERNING_BODY_TYPES).filter(key => !testTags[GOVERNING_BODY_TYPES[key].label]).map(key => (
+									{
+										label: GOVERNING_BODY_TYPES[key].label,
+										translation: translate[GOVERNING_BODY_TYPES[key].label],
+										type: 1
+									}
+								))}
 							/>
-							<ContenedorEtiquetas
-								color={'#7f94b6'}
-								stylesContent={{
-									border: '1px solid #7f94b6',
-									color: '#7f94b6',
-								}}
-								last={true}
-							/>
+
+							{!!draftTypes &&
+								<ContenedorEtiquetas
+									color={'#7fa5b6'}
+									addTag={addTag}
+									title={translate.draft_type}
+									stylesContent={{
+										border: '1px solid #7fa5b6',
+										color: '#7fa5b6',
+									}}
+									tags={draftTypes.map(draft => (
+										{
+											label: draft.label,
+											translation: translate[draft.label],
+											type: 2,
+											childs: draft.label === 'agenda'?
+												CBX.filterAgendaVotingTypes(votingTypes)
+												.filter(type => !testTags[type.label])
+												.map(votingType => {
+													return (
+														<Etiqueta
+															key={`tag_${votingType.value}`}
+															childs={CBX.hasVotation(votingType.value)?
+																majorityTypes
+																.filter(majority => {
+																	return !testTags[formatTagLabel({
+																		label: majority.label,
+																		segments: [draft.label, votingType.label, majority.label],
+																})]})
+																.map(majority => {
+																	return (
+																		<Etiqueta
+																			key={`tag_${majority.value}`}
+																			text={translate[majority.label]}
+																			color={getTagColor(draft.value)}
+																			action={() => addTag({
+																				label: majority.label,
+																				segments: [draft.label, votingType.label, majority.label],
+																				translation: translate[majority.label],
+																				type: 2,
+																			})}
+																		/>
+																	)
+																}) : null}
+															text={translate[votingType.label]}
+															color={getTagColor(draft.value)}
+															action={() => addTag({
+																label: votingType.label,
+																segments: [draft.label, votingType.label],
+																translation: translate[votingType.label],
+																type: 2,
+															})}
+														/>
+													)
+												}) : null
+										}
+									))}
+								/>
+							}
 						</div>
 						<div style={{ marginBottom: "1em" }}>
 							<TextInput
 								type="text"
+								placeholder='Crear etiqueta nueva'//TRADUCCION
 								value={newTag}
+								onKeyUp={handleEnter}
 								styleInInput={{ minHeight: '2.5em', paddingLeft: "0.4em", paddingRight: "0.4em", fontSize: "12px", color: "rgba(0, 0, 0, 0.36)", background: "#f2f4f7" }}
 								onChange={event => {
 									setNewTag(event.target.value);
@@ -279,7 +406,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 									</MenuItem>
 								}
 								text={translate.add_agenda_point}
-								textStyle={"ETIQKETA"}
+								textStyle={"ETIQUETA"}
 								// anchorOrigin={{
 								// 	vertical: 'bottom',
 								// 	horizontal: 'left',
@@ -368,7 +495,6 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 
 													</div>
 												</GridItem>
-											
 											</Grid>
 										</div>
 									</div>
@@ -422,7 +548,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 								{renderEtiquetasSeleccionadas()}
 							</GridItem>
 							<GridItem xs={12} lg={12} md={12}>
-								{renderDescripcion()}
+								{renderDescription()}
 							</GridItem>
 							<GridItem xs={12} lg={12} md={12}>
 								{renderRichEditor()}
@@ -433,7 +559,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 						{renderSelectorEtiquetas()}
 					</GridItem>
 				</Grid>
-			</Scrollbar >
+			</Scrollbar>
 		);
 	} else {
 		return (
@@ -451,7 +577,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 								{renderEtiquetasSeleccionadas()}
 							</GridItem>
 							<GridItem xs={12} lg={8} md={8}>
-								{renderDescripcion()}
+								{renderDescription()}
 							</GridItem>
 							<GridItem xs={12} lg={8} md={8}>
 								{renderRichEditor()}
@@ -466,7 +592,7 @@ const CompanyDraftForm = ({ translate, draft, errors, updateState, companyStatut
 }
 
 
-const ContenedorEtiquetas = ({ stylesContent, color, last }) => {
+const ContenedorEtiquetas = ({ stylesContent, color, last, title, tags, addTag, translate }) => {
 	const [open, setOpen] = React.useState(false);
 
 	return (
@@ -481,7 +607,7 @@ const ContenedorEtiquetas = ({ stylesContent, color, last }) => {
 		}}
 		>
 			<div style={{ alignItems: "center", justifyContent: "space-between", display: "flex", width: "100%", cursor: "pointer", }} onClick={() => setOpen(!open)}>
-				<div>Tipo de Runion</div>
+				<div>{title}</div>
 				<div style={{ display: "flex", alignItems: "center" }}>
 					{open ?
 						<i className="material-icons" style={{ fontSize: "40px" }}>
@@ -497,61 +623,26 @@ const ContenedorEtiquetas = ({ stylesContent, color, last }) => {
 			<Collapse in={open} timeout="auto" unmountOnExit >
 				<div style={{ marginBottom: "1em" }}>
 					<div style={{}}>
-						<Etiqueta
-							text={"Cabecera de convocatoria"}
-							color={color}
-						/>
-						<Etiqueta
-							text={"Cabecera"}
-							color={color}
-						/>
-						<Etiqueta
-							text={"Convocatoria"}
-							color={color}
-							moreEtiquetas={true}
-							etiquetas={
-								<React.Fragment>
-									<Etiqueta
-										text={"Cabecera"}
-										color={color}
-									/>
-									<Etiqueta
-										text={"Votacion Nominal"}
-										color={color}
-										width={'60%'}
-										moreEtiquetas={true}
-										etiquetas={
-											<React.Fragment>
-												<Etiqueta
-													text={"Cabecera3"}
-													color={color}
-												/>
-												<Etiqueta
-													text={"Votacion Nominal4"}
-													color={color}
-													width={'60%'}
-													moreEtiquetas={true}
-												/>
-											</React.Fragment>
-										}
-									/>
-								</React.Fragment>
-							}
-						/>
+						{tags.map((tag, index) => (
+							<Etiqueta
+								key={`tag_${index}`}
+								childs={tag.childs}
+								text={tag.translation? tag.translation : tag.label}
+								color={getTagColor(tag.type)}
+								action={() => addTag(tag)}
+							/>
+						))}
 
 					</div>
-					{/* <div style={{ boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.25)', border: `solid 1px ${color}`, borderRadius: '4px' }}>
-						<div style={{}}>Informativo</div>
-					</div> */}
 				</div>
 			</Collapse>
 
-		</div >
+		</div>
 	);
 }
 
 
-const Etiqueta = ({ text, color, moreEtiquetas, width, etiquetas }) => {
+const Etiqueta = ({ text, color, childs, width, etiquetas, addTag, action }) => {
 	const [open, setOpen] = React.useState(false);
 	const [openTimeOut, setOpenTimeOut] = React.useState(false);
 
@@ -563,15 +654,18 @@ const Etiqueta = ({ text, color, moreEtiquetas, width, etiquetas }) => {
 
 	const styles = {
 		borderRadius: '14px',
-		border: `solid 1px ${color}`,
+		border: `solid 1px`,
+		borderColor: color,
 		padding: "4px 0.8em",
+		cursor: 'pointer',
 		display: openTimeOut ? width ? "inline-block" : "block" : "inline-block",
 		marginRight: "0.5em",
 		marginTop: "0.25em",
 		marginBottom: "0.25em",
 		width: open && width
 	}
-	if (moreEtiquetas) {
+
+	if (childs) {
 		return (
 			<div style={{ ...styles }} >
 				<div style={{}}>
@@ -589,20 +683,10 @@ const Etiqueta = ({ text, color, moreEtiquetas, width, etiquetas }) => {
 							}
 						</div>
 					</div>
-					{moreEtiquetas &&
+					{childs &&
 						<Collapse in={open} timeout="auto" unmountOnExit >
 							<div>
-								{etiquetas}
-								{/* <Etiqueta
-									text={"Cabecera"}
-									color={color}
-								/>*/}
-								{/* <Etiqueta
-									text={"Votacion Nominal"}
-									color={color}
-									width={'60%'}
-									moreEtiquetas={true}
-								/>  */}
+								{childs}
 							</div>
 						</Collapse>
 					}
@@ -611,7 +695,7 @@ const Etiqueta = ({ text, color, moreEtiquetas, width, etiquetas }) => {
 		)
 	} else {
 		return (
-			<div style={{ ...styles }}>
+			<div style={{ ...styles }} onClick={action}>
 				{text}
 			</div>
 		)
@@ -619,30 +703,64 @@ const Etiqueta = ({ text, color, moreEtiquetas, width, etiquetas }) => {
 
 }
 
-const EtiquetaBase = ({ text, color }) => {
+const EtiquetaBase = ({ text, color, action }) => {
+	const anchoRef = React.useRef();
+	const [tooltip, setTooltip] = React.useState(false);
+
+	React.useLayoutEffect(() => {
+		if(anchoRef.current.clientWidth > (15 * 12) && !tooltip){
+			setTooltip(true);
+		}
+	});
 
 	return (
-		<div
-			style={{
-				borderRadius: '20px',
-				border: `solid 1px ${color}`,
-				background: color,
-				padding: "0 0.5em",
-				display: "inline-block",
-				marginRight: "0.5em",
-				marginTop: "0.25em",
-				marginBottom: "0.25em",
-				color: "white",
-				padding: "8px"
-			}}
-		>
-			<div style={{ display: "flex" }}>
-				<div style={{ paddingRight: "0.5em" }}>{/*text*/}Junta General Extraod.</div>
-				<div><i className="fa fa-times" style={{ cursor: 'pointer', background: " #ffffff", color: color, borderRadius: "6px", padding: "0em 1px" }} aria-hidden="true"></i></div>
+		<React.Fragment>
+			<div style={{ visibility: 'hidden', position: 'absolute' }} ref={anchoRef}>{text}</div>
+			<div
+				style={{
+					borderRadius: '20px',
+					background: color,
+					padding: "0 0.5em",
+					display: "inline-block",
+					marginRight: "0.5em",
+					marginTop: "0.25em",
+					marginBottom: "0.25em",
+					color: "white",
+					padding: "8px"
+				}}
+			>
+				<div style={{ display: "flex", justifyContent: 'space-between' }}>
+					{tooltip?
+						<Tooltip title={text}>
+							<div style={{ paddingRight: "0.5em", maxWidth: '15em' }} className="truncate">{text}</div>
+						</Tooltip>
+					:
+						<div style={{ paddingRight: "0.5em", maxWidth: '15em' }} className="truncate">{text}</div>
+					}
+					<div>
+						<i
+							className="fa fa-times"
+							style={{ cursor: 'pointer', background: " #ffffff", color, borderRadius: "6px", padding: "0em 1px" }}
+							aria-hidden="true"
+							onClick={action}
+						>
+						</i>
+					</div>
+				</div>
 			</div>
-		</div>
+		</React.Fragment>
 	)
+}
 
+const getTagColor = type => {
+	const colors = {
+		0: 'rgba(125, 33, 128, 0.58)',
+		1: 'rgba(33, 98, 128, 0.58)',
+		2: 'rgba(33, 70, 128, 0.58)',
+		99: 'rgba(128, 78, 33, 0.58)'
+	}
+
+	return colors[type]? colors[type] : colors[99];
 }
 
 
