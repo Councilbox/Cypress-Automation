a1d9fa2ace346cbb9e4eb4dd0f34cf9d13e6a16a
anxo <anxo.rodriguez@councilbox.com>
2019-09-25 08:52:58 +0200

    Auto stash before merge of "reunionFinalizada" and "origin/reunionFinalizada"

--- src/components/participant/login/ContactForm.js
+++ src/components/participant/login/ContactForm.js
@@ -12,11 +12,13 @@ const ContactForm = ({ participant, translate, council, client }) => {
         subject: '',
         body: ''
     });
+    const [errors, setErrors] = React.useState({});
     const primary = getPrimary();
 
     const send = async () => {
-        const response = await client.mutate({
-            mutation: gql`
+        if (!validate(state)) {
+            const response = await client.mutate({
+                mutation: gql`
                 mutation sendAdminEmail(
                     $councilId: Int!,
                     $message: AdminMessageInput
@@ -30,13 +32,51 @@ const ContactForm = ({ participant, translate, council, client }) => {
                     }
                 }
             `,
-            variables: {
-                councilId: council.id,
-                message: state
-            }
-        });
+                variables: {
+                    councilId: council.id,
+                    message: state
+                }
+            });
+            console.log(response);
+        }
+
     }
 
+    const validate = (items) => {
+        let hasError = false;
+        // console.log(items); ///object para array object.key(items)
+        console.log(state)
+        // state.map(item => (console.log(item))) 
+        // let newErrors = { items: Object.key(state).map(item => ({ error: '' })) }
+        let newErrors = {}
+
+        if (!state.body) {
+            newErrors.body = translate.required_field;
+            hasError = true;
+        }
+        if (!state.replyTo) {
+            newErrors.replyTo = translate.required_field;
+            hasError = true;
+        } else {
+            if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(state.replyTo))) {
+                newErrors.replyTo = "El email esta mal";
+                hasError = true;
+            }
+        }
+        if (!state.subject) {
+            newErrors.subject = translate.required_field;
+            hasError = true;
+        }
+
+        setErrors(newErrors);
+        return hasError;
+    }
+
+    // Estética como las plantillas
+    // Validación de campos vacíos
+    // Control de envio -> enviando -> enviado (la animación esa que hiciste)
+
+    console.log(errors)
 
     return (
         <React.Fragment>
@@ -44,18 +84,21 @@ const ContactForm = ({ participant, translate, council, client }) => {
                 value={state.replyTo}
                 onChange={event => setState({ replyTo: event.target.value })}
                 floatingText={'Email'}
+                errorText={errors.replyTo}
             />
             <TextInput
                 value={state.subject}
                 onChange={event => setState({ subject: event.target.value })}
                 floatingText={translate.title}
+                errorText={errors.subject}
             />
             <RichTextInput
                 value={state.body}
                 onChange={value => setState({ body: value })}
                 floatingText={translate.message}
+                errorText={errors.body}
             />
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end', marginTop: '0.6em'}}>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end', marginTop: '0.6em' }}>
                 <BasicButton
                     text={translate.send}
                     color={primary}
