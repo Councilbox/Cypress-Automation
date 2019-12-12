62bef53b050286e7107896c59334e970d02e5e7a
anxo <anxo.rodriguez@councilbox.com>
2019-12-12 13:08:13 +0100

    fix minimo page

--- src/components/documentEditor/DocumentEditor.js
+++ src/components/documentEditor/DocumentEditor.js
@@ -663,9 +663,10 @@ const Timbrado = ({ colapse, edit }) => {
 
     React.useEffect(() => {
         if (document.getElementsByClassName("actaLienzo")[0]) {
-            if (Math.ceil(document.getElementsByClassName("actaLienzo")[0].scrollHeight / 995) >= 2) {
+            //el if es para que minimo monte 2 paginas
+            // if (Math.ceil(document.getElementsByClassName("actaLienzo")[0].scrollHeight / 995) >= 2) {
                 setImgIzqCbx(Math.ceil(document.getElementsByClassName("actaLienzo")[0].scrollHeight / 995))
-            }
+            // }
         }
     }, [document.getElementsByClassName("actaLienzo")[0], colapse, edit])
 
