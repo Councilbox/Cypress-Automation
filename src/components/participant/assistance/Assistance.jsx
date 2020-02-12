bf02ed7e0e5088b27e4c60202c4deda232b8b2a9
anxo <anxo.rodriguez@councilbox.com>
2020-02-12 16:16:01 +0100

    wip

--- src/components/participant/assistance/Assistance.jsx
+++ src/components/participant/assistance/Assistance.jsx
@@ -7,20 +7,21 @@ import AssistanceOption from "./AssistanceOption";
 import { compose, graphql } from "react-apollo";
 import { setAssistanceIntention, setAssistanceComment } from "../../../queries/liveParticipant";
 import { PARTICIPANT_STATES, PARTICIPANT_TYPE } from "../../../constants";
-import { BasicButton, ButtonIcon, NotLoggedLayout, Scrollbar, DateWrapper, SectionTitle, LiveToast, DropDownMenu } from '../../../displayComponents';
+import { BasicButton, ButtonIcon, NotLoggedLayout, Scrollbar, DateWrapper, SectionTitle, LiveToast, DropDownMenu, AlertConfirm, GridItem, Grid, ReactSignature } from '../../../displayComponents';
 import RichTextInput from "../../../displayComponents/RichTextInput";
 import DelegateOwnVoteAttendantModal from "./DelegateOwnVoteAttendantModal";
 import RefuseDelegationConfirm from '../delegations/RefuseDelegationConfirm';
 import NoAttendDelegationWarning from '../delegations/NoAttendDelegationWarning';
 import DelegationItem from "./DelegationItem";
 import { canDelegateVotes } from "../../../utils/CBX"
-import { primary, secondary, getPrimary } from "../../../styles/colors";
+import { primary, secondary, getPrimary, getSecondary } from "../../../styles/colors";
 import { toast } from 'react-toastify';
 import { participantsToDelegate } from "../../../queries";
 import AddRepresentativeModal from "../../council/live/AddRepresentativeModal";
 import emptyMeetingTable from "../../../assets/img/empty_meeting_table.png";
 import { CONSENTIO_ID } from "../../../config";
 import MenuSuperiorTabs from "../../dashboard/MenuSuperiorTabs";
+import withWindowSize from "../../../HOCs/withWindowSize";
 
 
 const styles = {
@@ -54,16 +55,23 @@ const styles = {
 	}
 };
 
-const Assistance = ({ participant, data, translate, council, company, refetch, setAssistanceComment, setAssistanceIntention }) => {
+const Assistance = ({ participant, data, translate, council, company, refetch, innerWidth, setAssistanceComment, setAssistanceIntention }) => {
 	const [state, setState] = React.useState({
 		participant: {},
 		savingAssistanceComment: false,
 		delegationModal: false,
 		addRepresentative: false,
 		noAttendWarning: false,
+		clean: true,
 		...generateAttendanceData()
 	});
+	const [canvasSize, setCanvasSize] = React.useState({
+		width: 0,
+		height: 0
+	})
 	const [selecteAssistance, setSelecteAssistance] = React.useState('Reunión actual');
+	const [openModalFirmasModal, setOpenModalFirmasModal] = React.useState(false);
+	const signature = React.useRef(null);
 
 	function generateAttendanceData() {
 		if (participant.represented && participant.represented.length > 0) {
@@ -151,7 +159,25 @@ const Assistance = ({ participant, data, translate, council, company, refetch, s
 			await refetch();
 		}
 	}
+	const square_ref = React.useRef();
 
+	React.useEffect(() => {
+		let timeout = setTimeout(() => {
+			if (square_ref.current) {
+				setCanvasSize({
+					width: (square_ref.current.getBoundingClientRect().width - 20),
+					height: (square_ref.current.getBoundingClientRect().height - 110),
+				})
+			}
+		}, 150)
+		return () => clearTimeout(timeout);
+
+	}, [openModalFirmasModal, innerWidth])
+
+	const openModalFirmas = async () => {
+		setOpenModalFirmasModal(true);
+		// sendAttendanceIntention();
+	}
 
 	const sendAttendanceIntention = async () => {
 		setState({
@@ -250,6 +276,7 @@ const Assistance = ({ participant, data, translate, council, company, refetch, s
 			</NotLoggedLayout>
 		)
 	}
+
 
 	const getReunionActual = () => {
 		return (
@@ -550,7 +577,8 @@ const Assistance = ({ participant, data, translate, council, company, refetch, s
 										fontWeight: "700"
 									}}
 									loading={state.loading}
-									onClick={sendAttendanceIntention}
+									onClick={openModalFirmas}
+									// onClick={sendAttendanceIntention}
 									icon={<ButtonIcon type="save" color="white" />}
 								/>
 							}
@@ -564,6 +592,112 @@ const Assistance = ({ participant, data, translate, council, company, refetch, s
 					addRepresentative={selectDelegation}
 					requestClose={() => setState({ ...state, delegationModal: false })}
 					translate={translate}
+				/>
+				{/* ///aki */}
+				<AlertConfirm
+					open={openModalFirmasModal}
+					bodyStyle={{
+						width: "50vw",
+						// minWidth: "50vw",
+					}}
+					requestClose={() => setOpenModalFirmasModal(false)}
+					title={"Generación de documento delegado"}
+					bodyText={
+						<Grid style={{ marginTop: "15px", height: "100%" }}>
+							<GridItem xs={12} md={6} lg={6} style={{ height: "100%" }} >
+								<div style={{
+									boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.5)',
+									padding: "1em",
+									color: "#000000",
+									height: "100%"
+								}}>
+									<div>{council.company.businessName}</div>
+									<div>{council.street}</div>
+									<div>{council.countryState} {council.countryState}</div>
+									<div>{council.country}</div>
+
+									<div>En Salamanca, a 05 de Febrero de 2020</div>
+
+									<div>Distinguido/s Señor/es:</div>
+
+									<div>
+										No pudiendo asistir a la Junta General Extraordinaria de
+									Accionistas  de OLIVO ENTERPRISE, S.A.
+									convocada para el próximo día 14 de Marzo de
+									2020 a las 12:34 horas, en C/ Martin,
+									en primera convocatoria, o bien el 14 de Marzo de 2020 a las 13.00
+									horas en el mismo lugar, en segunda convocatoria,
+									delego mi representación y voto en favor de D./ Aaron Fuentes Garcia para que me
+									represente en dicha reunión sin limitación de facultad de voto.
+									</div>
+
+									<div>Le saluda muy atentamente,</div>
+									_________________________________
+									<div>D.  {participant.name} {participant.surname} </div>
+								</div>
+							</GridItem>
+							<GridItem xs={12} md={6} lg={6} >
+								<div style={{
+									borderRadius: '4px',
+									boxShadow: ' 0 2px 4px 0 rgba(0, 0, 0, 0.5)',
+									color: getPrimary(),
+									marginBottom: "1em",
+									padding: "0.6em 1em",
+									display: "flex",
+									alignItems: "center",
+									width: "100%"
+								}}>
+									<div style={{
+										whiteSpace: 'nowrap',
+										overflow: 'hidden',
+										textOverflow: 'ellipsis',
+										cursor: "pointer"
+									}}>Imprimir PDF para firmar en papel</div>
+									<i className="material-icons" style={{
+										color: getPrimary(),
+										fontSize: '14px',
+										paddingRight: "0.3em",
+										cursor: "pointer",
+										marginLeft: "5px"
+									}} >
+										help
+									</i>
+								</div>
+								<div
+									style={{
+										border: 'solid 1px #979797',
+										color: '#a09aa0',
+										padding: "0.6em",
+										marginBottom: "1em",
+										height: 'calc( 100% - 7em )'
+									}}
+									ref={square_ref}
+								>
+									Firme en este recuadro para hacer efectiva la firma electrónica en el documento.
+									<ReactSignature
+										height={canvasSize.height}
+										width={canvasSize.width}
+										dotSize={1}
+										onEnd={() => setState({
+											...state,
+											clean: false
+										})}
+										style={{}}
+										ref={signature}
+									/>
+								</div>
+								<BasicButton
+									text={'Enviar documento firmado'}
+									color={getSecondary()}
+									textStyle={{
+										color: "white",
+										width: "100%"
+									}}
+								// onClick={sendAttendanceIntention}
+								/>
+							</GridItem>
+						</Grid>
+					}
 				/>
 			</div>
 		)
@@ -1130,15 +1264,17 @@ const DelegationSection = ({ participant, translate, refetch }) => {
 
 
 
-export default compose(graphql(setAssistanceIntention, {
-	name: "setAssistanceIntention"
-}), graphql(setAssistanceComment, {
-	name: "setAssistanceComment"
-}), graphql(participantsToDelegate, {
-	options: props => ({
-		variables: {
-			councilId: props.council.id
-		}
-	})
-})
+export default compose(
+	graphql(setAssistanceIntention, {
+		name: "setAssistanceIntention"
+	}), graphql(setAssistanceComment, {
+		name: "setAssistanceComment"
+	}), graphql(participantsToDelegate, {
+		options: props => ({
+			variables: {
+				councilId: props.council.id
+			}
+		})
+	}),
+	withWindowSize
 )(withTranslations()(Assistance));
