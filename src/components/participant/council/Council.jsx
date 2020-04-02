0512935f9635cebca37b6768023e65e1e8957bd1
Aaron Fuentes <aaron.fuentes@cocodin.com>
2020-04-02 11:26:44 +0200

    close #680

--- src/components/participant/council/Council.jsx
+++ src/components/participant/council/Council.jsx
@@ -7,7 +7,7 @@ import withDetectRTC from "../../../HOCs/withDetectRTC";
 import { PARTICIPANT_STATES } from '../../../constants';
 import Agendas from '../agendas/Agendas';
 import Header from "../Header";
-import { darkGrey } from '../../../styles/colors';
+import { darkGrey, getPrimary } from '../../../styles/colors';
 import RequestWordMenu from '../menus/RequestWordMenu';
 import { councilHasVideo } from '../../../utils/CBX';
 import { isLandscape, isMobile } from '../../../utils/screen';
@@ -234,6 +234,29 @@ const ParticipantCouncil = ({ translate, participant, council, client, ...props 
     }
 
     const renderVideoContainer = () => {
+        if(participant.requestWord === 3){
+            //TRADUCCION
+            return (
+                <div style={{
+                    backgroundColor: 'white',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '1em'
+                }}>
+                    <span className="material-icons" style={{ color: getPrimary(), fontSize: '50px' }}>
+                        tv_off
+                    </span>
+                    <div style={{textAlign: 'center'}}>En la <strong style={{ color: getPrimary(), marginRight: '0.5em' }}>sala de espera.</strong>
+                        Podrá acceder a la emisión cuando el administrador de sala le conceda la entrada.
+                    </div>
+                </div>
+            )
+        }
+
         return (
             <VideoContainer
                 council={council}
