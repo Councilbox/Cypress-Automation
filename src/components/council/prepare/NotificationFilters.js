c87bd76f847fcf2447db344cbcbf1bddcc71c4fa
Anxo <anxo.rodriguez.cocodin@gmail.com>
2020-06-05 12:46:44 +0200

    filtros solo cuando asistencia

--- src/components/council/prepare/NotificationFilters.js
+++ src/components/council/prepare/NotificationFilters.js
@@ -95,14 +95,14 @@ class NotificationFilters extends React.Component {
 				active={selectedFilter === value}
 				tooltip={translate[CBX.getAttendanceIntentionTooltip(value)]}
 			>
-				<i className={CBX.getAttendanceIntentionIcon(value)} style={{width: "24px", height: "auto", color: primary }}></i>
+				<i className={CBX.getAttendanceIntentionIcon(value)} style={{ width: "24px", height: "auto", color: primary }}></i>
 			</FilterButton>
 		);
 	}
 
 	render() {
 		const { translate } = this.props;
-
+		
 		return (
 			<Grid>
 				<GridItem xs={4} md={9} lg={3} style={{ paddingTop: "0.6em" }}>
@@ -120,9 +120,11 @@ class NotificationFilters extends React.Component {
 					{Object.keys(EMAIL_STATES_FILTERS).map(code =>
 						this._renderFilterIcon(EMAIL_STATES_FILTERS[code])
 					)}
-					{intentionStates.map(intention => (
-						this._renderIntentionIcon(intention)
-					))}
+					{CBX.councilHasAssistanceConfirmation(this.props.council) &&
+						intentionStates.map(intention => (
+							this._renderIntentionIcon(intention)
+						))
+					}
 				</GridItem>
 			</Grid>
 		);
