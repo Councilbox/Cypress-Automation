ff5492f674ac2f0e8fcfcff4b3e0d8f9fde49269
Anxo <anxo.rodriguez.cocodin@gmail.com>
2020-08-05 17:40:55 +0200

    fix

--- src/config.js
+++ src/config.js
@@ -24,8 +24,7 @@ export const variant = getVariant();
 export const EXPLORER_URL = 'https://alastria-explorer.councilbox.com';
 
 export const CLIENT_VERSION = process.env.REACT_APP_VERSION || '6.7.1';
-export const API_URL = 'https://api.councilbox.com/graphql';
-// export const API_URL = `${SERVER_URL}/graphql`;
+export const API_URL = `${SERVER_URL}/graphql`;
 export const videoVersions = {
 	CMP: 'CMP',
 	OLD: 'OLD'
