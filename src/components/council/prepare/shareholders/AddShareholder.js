d8adb0536c8390a2665615db891007363942de02
Aaron Fuentes <aaron.fuentes@cocodin.com>
2020-10-09 14:10:59 +0200

    Force int value for numParticipations in requests

--- src/components/council/prepare/shareholders/AddShareholder.js
+++ src/components/council/prepare/shareholders/AddShareholder.js
@@ -9,6 +9,7 @@ import { getSecondary } from '../../../../styles/colors';
 const ApproveRequestButton = ({ request, client, refetch, translate, council }) => {
     const [modal, setModal] = React.useState(null);
     let { requestType, attachments, earlyVotes, representative, ...cleanData } = request.data;
+    cleanData.numParticipations = +cleanData.numParticipations || 1;
     cleanData.socialCapital = cleanData.numParticipations || 1;
     cleanData.personOrEntity = cleanData.personOrEntity ? +cleanData.personOrEntity : 0;
     const secondary = getSecondary();
