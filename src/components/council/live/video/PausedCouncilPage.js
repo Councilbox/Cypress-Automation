d0389865f3945f861555549d57eae90baa0a6df2
Aaron Fuentes <aaron.fuentes@cocodin.com>
2020-11-12 12:33:14 +0100

    close #920

---
+++ src/components/council/live/video/PausedCouncilPage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
+import { usePolling } from '../../../../hooks';
+import { getPrimary } from '../../../../styles/colors';
+
+
+const PausedCouncilPage = ({ council, client, translate }) => {
+    const [message, setMessage] = React.useState('');
+    const primary = getPrimary();
+
+    const getData = React.useCallback(async () => {
+        const response = await client.query({
+            query: gql`
+                query CouncilPausedMessage($councilId: Int!){
+                    councilPausedMessage(councilId: $councilId)
+                }
+            `,
+            variables: {
+                councilId: council.id
+            }
+        });
+
+        console.log(response);
+        setMessage(response.data.councilPausedMessage);
+    }, [council.id]);
+
+    React.useEffect(() => {
+        getData();
+    }, [getData])
+
+    usePolling(getData, 3000);
+
+
+    return (
+        <div style={{
+            width: '100%',
+            height: '100%',
+            position: 'relative',
+            backgroundColor: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <div style={{ textAlign: 'center' }}>
+                <h5 style={{ color: primary, fontWeight: '700' }}>REUNIÓN PAUSADA</h5>
+                <div
+                    dangerouslySetInnerHTML={{ __html: message }}
+                ></div>
+            </div>
+        </div>
+    )
+}
+
+export default withApollo(PausedCouncilPage);
