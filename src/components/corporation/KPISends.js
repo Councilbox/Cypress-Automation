23de3ed2c6f219cf1faa99c76c2d1f6941551fc8
Aaron Fuentes <aaron.fuentes@cocodin.com>
2020-11-24 11:18:57 +0100

    WIP #962

---
+++ src/components/corporation/KPISends.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
+
+
+const KPISends = ({ client, translate, councilId }) => {
+
+    return (
+        <div>
+            PRUEBA
+        </div>
+    )
+}
+
+export default withApollo(KPISends)
