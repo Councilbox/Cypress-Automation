3987a735cb3faa5926cd949d8001f255cf68b035
Aaron Fuentes <aaron.fuentes@cocodin.com>
2020-11-27 17:43:25 +0100

    Added code to updateUser

--- src/components/userSettings/UserForm.js
+++ src/components/userSettings/UserForm.js
@@ -9,7 +9,7 @@ import { MenuItem } from 'material-ui';
 import { getPrimary } from '../../styles/colors';
 import { isMobile } from '../../utils/screen';
 
-const UserForm = ({ data, updateState, errors, languages, onKeyUp, translate }) => (
+const UserForm = ({ data, updateState, errors, languages, onKeyUp, translate, admin = false }) => (
     <Grid style={{ justifyContent: "space-between", color: '#61abb7' }}>
         <GridItem xs={12} md={12} lg={12} style={{ display: "flex", alignItems: 'center', }}>
             <div style={{ width: "120px", color: getPrimary(), fontWeight: "bold" }}>
@@ -75,6 +75,29 @@ const UserForm = ({ data, updateState, errors, languages, onKeyUp, translate }) 
                 />
             </div>
         </GridItem>
+        {admin &&
+            <GridItem xs={12} md={12} lg={12} style={{ display: "flex", alignItems: 'center', }}>
+                <div style={{ width: "120px", color: getPrimary(), fontWeight: "bold" }}>
+                    {translate.code}
+                </div>
+                <div>
+                    <TextInput
+                        type="text"
+                        value={data.code}
+                        disableUnderline={!!data.code}
+                        styles={{ fontWeight: "bold", width: isMobile ? "100%" : '300px' }}
+                        {...(!!onKeyUp ? { onKeyUp: onKeyUp } : {})}
+                        onChange={event =>
+                            updateState({
+                                code: event.target.value
+                            })
+                        }
+                        errorText={errors.code}
+                    // required
+                    />
+                </div>
+            </GridItem>
+        }
         <GridItem xs={12} md={12} lg={12} style={{ display: "flex", alignItems: 'center', }}>
             <div style={{ display: "flex", alignItems: 'center', justifyContent: "space-between", width: "100%" }}>
                 <div style={{ display: "flex", alignItems: 'center', }}>
@@ -164,128 +187,11 @@ const UserForm = ({ data, updateState, errors, languages, onKeyUp, translate }) 
                                     </MenuItem>
                                 ))}
                         </SelectInput>
-                        {/* <span style={{ color: getPrimary() }} className="material-icons">
-                            arrow_drop_down
-                        </span> */}
                     </div>
                 </div>
             </GridItem>
         }
         <div style={{ width: "100%", border: "1px solid" + getPrimary() }}></div>
-        {/* <GridItem xs={12} md={12} lg={12} style={{ display: "flex", alignItems: 'center', }}>
-            IDIOMA
-        </GridItem> */}
-
-
-        {/* <GridItem xs={12} md={12} lg={12}>
-            <h2 style={{
-                color: getPrimary(),
-                fontSize: '18px',
-                fontWeight: '700',
-                marginBottom: '0.6em'
-            }}>
-               
-                Datos del nuevo usuario
-            </h2>
-        </GridItem> */}
-        {/* <GridItem xs={12} md={6} lg={5} style={{ display: "flex", alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ fontSize: "15px", width: "2em" }}>
-                <i className={'fa fa-info'}></i>
-            </div>
-            <div style={{ width: "100%" }}>
-                <TextInput
-                    floatingText={translate.name}
-                    type="text"
-                    required
-                    value={data.name}
-                    errorText={errors.name}
-                    onChange={event =>
-                        updateState({
-                            name: event.target.value
-                        })
-                    }
-                />
-            </div>
-        </GridItem>
-        <GridItem xs={12} md={6} lg={5} style={{ display: "flex", alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ fontSize: "15px", width: "2em" }}></div>
-            <div style={{ width: "100%" }}>
-                <TextInput
-                    floatingText={translate.surname || ''}
-                    type="text"
-                    value={data.surname || ''}
-                    onChange={event =>
-                        updateState({
-                            surname: event.target.value
-                        })
-                    }
-                    errorText={errors.surname || ''}
-                    required
-                />
-            </div>
-        </GridItem>
-        <GridItem xs={12} md={6} lg={5} style={{ display: "flex", alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ fontSize: "15px", width: "2em" }}>
-                <i className={'fa fa-at'}></i>
-            </div>
-            <div style={{ width: "100%" }}>
-                <TextInput
-                    floatingText={translate.email}
-                    type="text"
-                    value={data.email}
-                    {...(!!onKeyUp ? { onKeyUp: onKeyUp } : {})}
-                    onChange={event =>
-                        updateState({
-                            email: event.target.value
-                        })
-                    }
-                    errorText={errors.email}
-                    required
-                />
-            </div>
-        </GridItem>
-        <GridItem xs={12} md={6} lg={5} style={{ display: "flex", alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ fontSize: "15px", width: "2em" }}></div>
-            <div style={{ width: "100%" }}>
-                <TextInput
-                    floatingText={translate.phone}
-                    type="text"
-                    value={data.phone}
-                    errorText={errors.phone}
-                    onChange={event =>
-                        updateState({
-                            phone: event.target.value
-                        })
-                    }
-                />
-            </div>
-        </GridItem>
-        <GridItem xs={12} md={6} lg={5} style={{ display: "flex", alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{ fontSize: "15px", width: "2em" }}></div>
-            <div style={{ width: "100%" }}>
-                <SelectInput
-                    floatingText={translate.language}
-                    value={data.preferredLanguage}
-                    onChange={event =>
-                        updateState({
-                            preferredLanguage: event.target.value
-                        })
-                    }
-                    errorText={errors.preferredLanguage}
-                    required
-                >
-                    {languages &&
-                        languages.map(language => (
-                            <MenuItem
-                                key={`language_${language.columnName}`}
-                                value={language.columnName}
-                            >
-                                {language.desc}
-                            </MenuItem>
-                        ))}
-                </SelectInput>
-            </div>
-        </GridItem> */}
     </Grid >
 )
 
