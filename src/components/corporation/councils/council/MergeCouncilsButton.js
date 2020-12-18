fd2e6b7bf968aaa9eee7d0d70b6dbdd2530c5a6c
Aaron Fuentes <aaron.fuentes@cocodin.com>
2020-12-18 16:43:55 +0100

    close #1010

--- src/components/corporation/councils/council/MergeCouncilsButton.js
+++ src/components/corporation/councils/council/MergeCouncilsButton.js
@@ -2,30 +2,119 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { withApollo } from 'react-apollo';
 import FontAwesome from 'react-fontawesome';
-import { BasicButton, AlertConfirm, TextInput } from '../../../../displayComponents';
-import { number } from 'prop-types';
+import { BasicButton, AlertConfirm, TextInput, Checkbox, LoadingSection } from '../../../../displayComponents';
+
+const reducer = (state, action) => {
+
+    const actions = {
+        'LOADING': () => ({
+            ...state,
+            status: 'LOADING'
+        }),
+        'ERROR': () => ({
+            ...state,
+            status: 'ERROR',
+            error: action.payload
+        }),
+        'SUCCESS': () => ({
+            ...state,
+            status: 'SUCCESS',
+            error: null
+        })
+    }
+
+    return actions[action.type] ? actions[action.type]() : state;
+
+}
 
 
 const MergeCouncilsButton = ({ translate, color, council, client }) => {
     const [modal, setModal] = React.useState(false);
     const [from, setFrom] = React.useState(null);
+    const [force, setForce] = React.useState(false);
+    const [{ status, error }, dispatch] = React.useReducer(reducer, {
+        status: 'IDDLE'
+    });
+
+    const loading = status === 'LOADING';
+    const hasError = status === 'ERROR';
 
     const mergeCouncils = async () => {
+        dispatch({ type: 'LOADING' });
         const response = await client.mutate({
             mutation: gql`
-                mutation MergeCouncils($from: Int!, $to: Int!){
-                    mergeCouncils(from: $from, to: $to){
+                mutation MergeCouncils($from: Int!, $to: Int!, $force: Boolean){
+                    mergeCouncils(from: $from, to: $to, force: $force){
                         success
                     }
                 }
             `,
             variables: {
                 from,
-                to: council.id
+                to: council.id,
+                force
             }
         });
 
-        console.log(response);
+        if(response.errors){
+            if(response.errors[0].message = 'Repeated emails between councils'){
+                dispatch({ type: 'ERROR', payload: {
+                    type: 'Duplicated emails',
+                    repeatedEmails: response.errors[0].originalError.errors.repeatedEmails
+                }});
+            }
+        } else {
+            dispatch({ type: 'SUCCESS' });
+        }
+    }
+
+
+    const renderBody = () => {
+
+        if(status === 'SUCCESS'){
+            return (
+                'Reuniones mezcladas con éxito'
+            )
+        }
+
+        if(hasError){
+            return (
+                <>
+                    {error.type === 'Duplicated emails' && 
+                        <>
+                            Hay emails repetidos entre las reuniones
+                            {error.repeatedEmails.map(email => (
+                                <div key={email}>
+                                    {email}
+                                </div>
+                            ))}
+                            Forzar la mezcla igualmente omitiendo el participante con email repetido?
+                            <Checkbox
+								value={force}
+								onChange={() => setForce(!force)}
+							/>
+                        </>
+                    }
+
+                </>
+        
+            )
+        }
+
+        if(loading){
+            return <LoadingSection />
+        }
+
+        return (    
+            <TextInput
+                value={from}
+                type="number"
+                floatingText={'Mover participantes desde la ID'}
+                onChange={event => {
+                    setFrom(+event.target.value)
+                }}
+            />
+        )
     }
 
     return (
@@ -34,21 +123,11 @@ const MergeCouncilsButton = ({ translate, color, council, client }) => {
                 open={modal}
                 requestClose={() => setModal(false)}
                 acceptAction={mergeCouncils}
+                loadingAction={loading}
                 buttonAccept={translate.send}
                 title={'Mezclar reuniones'}
                 buttonCancel={translate.cancel}
-                bodyText={
-                    <>
-                        <TextInput
-                            value={from}
-                            type="number"
-                            floatingText={'Mover participantes desde la ID'}
-                            onChange={event => {
-                                setFrom(+event.target.value)
-                            }}
-                        />
-                    </>
-                }
+                bodyText={renderBody()}
             />
             <BasicButton
                 text={'Añadir participantes desde otra reunión'}
