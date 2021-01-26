9c766cf96c78e64f93a8df029eabe97201fccd97
Aaron Fuentes <aaron.fuentes@councilbox.com>
2021-01-26 09:48:01 +0100

    CU-d3064p[resolved]

--- src/utils/logRocket.js
+++ src/utils/logRocket.js
@@ -1,17 +1,30 @@
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
 
+const logRocketState = {
+    initialized: false
+}
+
 export const initLogRocket = user => {
+    if(logRocketState.initialized) {
+        return;
+    }
+
     if(process.env.REACT_APP_LOGROCKET_ENABLED){
         LogRocket.init(process.env.REACT_APP_LOGROCKET_ENABLED);
-        setupLogRocketReact(LogRocket);
+        try {
+            logRocketState.initialized = true;
+            setupLogRocketReact(LogRocket);
 
-        if(user.roles !== 'devAdmin'){
-            LogRocket.identify(user.id, {
-                name: `${user.name} ${user.surname}`,
-                email: user.email,
-                subscriptionType: user.roles || 'participant'
-            });
+            if(user.roles !== 'devAdmin'){
+                LogRocket.identify(user.id, {
+                    name: `${user.name} ${user.surname}`,
+                    email: user.email,
+                    subscriptionType: user.roles || 'participant'
+                });
+            }
+        } catch (error){
+            console.error(error);
         }
     }
 }
