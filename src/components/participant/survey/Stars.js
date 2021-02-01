8ace52510c8e917a88d1142d3c985ae5bd9fffc5
Aaron Fuentes <aaron.fuentes@councilbox.com>
2021-02-01 18:48:44 +0100

    Added disabled stars functionality

--- src/components/participant/survey/Stars.js
+++ src/components/participant/survey/Stars.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Stars = ({ name, lowStars, onClick, value, error }) => {
+const Stars = ({ name, lowStars, onClick, value, error, disabled = false }) => {
 	return (
 		<div style={{ display: "inline-block" }}>
 			{error && <span style={{ color: 'red', fontWeight: '700', marginBottom: '1em' }}>{error}</span>}
 			<form id={"ratingForm" + name}>
-				<fieldset className={"rating"}>
+				<fieldset className={disabled ? "rating-disabled" : "rating"}>
 					{[...new Array(5)].map((item, index) => (
 						<React.Fragment key={`${index}_${name}`}>
 							<input
@@ -14,8 +14,11 @@ const Stars = ({ name, lowStars, onClick, value, error }) => {
 								name={"rating2" + name}
 								checked={value === (5 - index)}
 								value={5 - index}
+								style={{ ...(disabled ? { cursor: 'default', onHover: '' } : {}) }}
 								onChange={event => {
-									onClick(event.target.value);
+									if(!disabled){
+										onClick(event.target.value);
+									}
 								}}
 							/>
 							<label htmlFor={"star" + (5 - index) + '' + name} className={lowStars ? 'lowStars' : ""}>5 stars</label>
