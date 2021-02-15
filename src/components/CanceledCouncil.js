98d1d694414cecb70e77532a3df6c5afb7e5d86b
Aaron Fuentes <aaron.fuentes@councilbox.com>
2021-02-15 19:13:08 +0100

    Cleaned lint error

--- src/components/CanceledCouncil.js
+++ src/components/CanceledCouncil.js
@@ -1,7 +1,6 @@
 import { Card, CardHeader } from 'material-ui';
 import React from 'react';
 import { NotLoggedLayout, Scrollbar } from '../displayComponents';
-import { getPrimary } from '../styles/colors';
 import { isMobile } from '../utils/screen';
 import { moment } from '../containers/App';
 
@@ -58,80 +57,74 @@ const styles = {
 };
 
 
-const CanceledCouncil = ({ council, translate }) => {
-	const primary = getPrimary();
-
-	console.log(council);
-
-	return (
-		<NotLoggedLayout
-			translate={translate}
-		>
-			<Scrollbar>
-				<div style={{
-					width: '100%',
-					height: '100%',
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}>
-					<Card style={{
-						...styles.cardContainer,
-						maxWidth: isMobile ? '100%' : '80%',
-						minHeight: '70vh',
-						minWidth: window.innerWidth > 450 ? '80%' : '100%',
-						padding: '2em'
-					}} elevation={6}>
+const CanceledCouncil = ({ council, translate }) => (
+	<NotLoggedLayout
+		translate={translate}
+	>
+		<Scrollbar>
+			<div style={{
+				width: '100%',
+				height: '100%',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}>
+				<Card style={{
+					...styles.cardContainer,
+					maxWidth: isMobile ? '100%' : '80%',
+					minHeight: '70vh',
+					minWidth: window.innerWidth > 450 ? '80%' : '100%',
+					padding: '2em'
+				}} elevation={6}>
+					<div style={{
+						...styles.loginContainerMax,
+						height: '',
+					}}>
 						<div style={{
 							...styles.loginContainerMax,
-							height: '',
+							...(council.securityType !== 0 ? {
+								height: ''
+							} : {}),
 						}}>
-							<div style={{
-								...styles.loginContainerMax,
-								...(council.securityType !== 0 ? {
-									height: ''
-								} : {}),
-							}}>
+							<React.Fragment>
 								<React.Fragment>
-									<React.Fragment>
-										<div
-											style={{
-												backgroundColor: 'gainsboro',
-												borderRadius: '4px'
-											}}
-										>
-											<CardHeader
-												title={
-													<div style={{ marginBottom: '10px' }}>
-														<b>{council.name}</b>
-													</div>
-												}
-												subheader={moment(new Date(council.dateStart)).format(
-													'LLL'
-												)}
-											/>
-										</div>
-									</React.Fragment>
-									<p style={{ marginBottom: '0px', fontSize: '2em', fontWeight: '700', marginTop: '1em' }}>
-										{translate.council_not_celebrated}
-									</p>
-									{council.noCelebrateComment &&
-										<div style={{ fontSize: '1.3em', marginTop: '1.2em' }}>
-											<p style={{ marginBottom: '0px' }}>
-												{translate.reason_not_held_council}:
-											</p>
-											{council.noCelebrateComment}
-										</div>
-									}
+									<div
+										style={{
+											backgroundColor: 'gainsboro',
+											borderRadius: '4px'
+										}}
+									>
+										<CardHeader
+											title={
+												<div style={{ marginBottom: '10px' }}>
+													<b>{council.name}</b>
+												</div>
+											}
+											subheader={moment(new Date(council.dateStart)).format(
+												'LLL'
+											)}
+										/>
+									</div>
 								</React.Fragment>
-							</div>
+								<p style={{ marginBottom: '0px', fontSize: '2em', fontWeight: '700', marginTop: '1em' }}>
+									{translate.council_not_celebrated}
+								</p>
+								{council.noCelebrateComment &&
+									<div style={{ fontSize: '1.3em', marginTop: '1.2em' }}>
+										<p style={{ marginBottom: '0px' }}>
+											{translate.reason_not_held_council}:
+										</p>
+										{council.noCelebrateComment}
+									</div>
+								}
+							</React.Fragment>
 						</div>
-					</Card>
-				</div>
-			</Scrollbar>
-		</NotLoggedLayout>
-	);
-};
+					</div>
+				</Card>
+			</div>
+		</Scrollbar>
+	</NotLoggedLayout>
+);
 
 export default CanceledCouncil;
