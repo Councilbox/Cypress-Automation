417e2ecd956d038a79ba6611879a2ff8a4048eec
Alem QA <alem@qaengineers.net>
2021-02-17 14:06:03 +0100

    AutomationQA

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -64,3 +64,15 @@ Cypress.Commands.add("logoutUI", () => {
     cy.contains('Logout').click();
     cy.url().should("include", login_url);
 });
+
+Cypress.Commands.add('iframe', { prevSubject: 'element' }, ($iframe, callback = () => {}) => {
+    // For more info on targeting inside iframes refer to this GitHub issue:
+    // https://github.com/cypress-io/cypress/issues/136
+    cy.log('Getting iframe body')
+
+    return cy
+        .wrap($iframe)
+        .should(iframe => expect(iframe.contents().find('body')).to.exist)
+        .then(iframe => cy.wrap(iframe.contents().find('body')))
+        .within({}, callback)
+})
