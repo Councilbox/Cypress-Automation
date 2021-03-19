6d1c4c6f42b4ec6b1c9f79aecef6458d6cb587d7
Alem QA <alem@qaengineers.net>
2021-03-19 13:16:16 +0100

    Fixing scenarios with new IDs.

--- cypress/integration/2.Regression/user_specs.js
+++ cypress/integration/2.Regression/user_specs.js
@@ -67,6 +67,7 @@ describe("Switcing back to English language", function() {
 */
 
 
+
 describe("The user is not able to register to the Councilbox without populating required fields", function() {
     
         it("Open the browser and enter the URL of the staging environment", function() {
@@ -1143,7 +1144,7 @@ describe("The user is able to edit 'Master key' in the 'Company settings' in the
     });
 
 
-describe("The user is able to edit 'External ID' in the 'Company settings' in the Councilbox - MISSING ID", function() {
+describe("The user is able to edit 'External ID' in the 'Company settings' in the Councilbox", function() {
 
         it("On the upper right corner click on the 'User icon' button", function() {
             cy.wait(5000)
@@ -1156,7 +1157,7 @@ describe("The user is able to edit 'External ID' in the 'Company settings' in th
 
 
         it("Modify the 'External ID' field and click on the 'Save' button", function() {
-            cy.get('#MISSING_ID').clear().type(userID_Alpha()+Cypress.config('UniqueNumber'))
+            cy.get('#company-external-id').clear().type(userID_Alpha()+Cypress.config('UniqueNumber'))
             cy.get('#save-button').click()
         });
 
@@ -1204,7 +1205,7 @@ describe("The user is able to edit 'Locality' in the 'Contact data' in the Compa
 
     });
 
-describe("The user is able to edit 'Country' in the 'Contact data' in the Company settings in the Councilbox - MISSING ID", function() {
+describe("The user is able to edit 'Country' in the 'Contact data' in the Company settings in the Councilbox", function() {
 
         it("On the upper right corner click on the 'User icon' button", function() {
             cy.wait(5000)
@@ -1217,7 +1218,9 @@ describe("The user is able to edit 'Country' in the 'Contact data' in the Compan
 
 
         it("Modify the 'Country' field and click on the 'Save' button", function() {
-            cy.get('#MISSING_ID').clear().type(userID_Alpha()+Cypress.config('UniqueNumber'))
+            cy.get('#company-country').click()
+            cy.wait(1000)
+            cy.xpath('(//*[@role="option"])[1]').click()
             cy.get('#save-button').click()
         });
 
@@ -1228,7 +1231,7 @@ describe("The user is able to edit 'Country' in the 'Contact data' in the Compan
 
     });
 
-describe("The user is able to edit 'Province' in the 'Contact data' in the Company settings in the Councilbox - MISSING ID", function() {
+describe("The user is able to edit 'Province' in the 'Contact data' in the Company settings in the Councilbox", function() {
 
         it("On the upper right corner click on the 'User icon' button", function() {
             cy.wait(5000)
@@ -1240,8 +1243,10 @@ describe("The user is able to edit 'Province' in the 'Contact data' in the Compa
         });
 
 
-        it("Modify the 'Province' field and click on the 'Save' button - MISSING ID", function() {
-            cy.get('#MISSING_ID').clear().type(userID_Alpha()+Cypress.config('UniqueNumber'))
+        it("Modify the 'Province' field and click on the 'Save' button", function() {
+            cy.get('#country-state-select').click()
+            cy.wait(1000)
+            cy.xpath('(//*[@role="option"])[1]').click()
             cy.get('#save-button').click()
         });
 
@@ -1271,7 +1276,7 @@ describe("The user is able to edit 'Zipcode' in the 'Contact data' in the Compan
 
     });
 
-describe("The user is able to edit 'Language' in the 'Contact data' in the Company settings in the Councilbox - MISSING ID", function() {
+describe("The user is able to edit 'Language' in the 'Contact data' in the Company settings in the Councilbox", function() {
 
         it("On the upper right corner click on the 'User icon' button", function() {
             cy.wait(5000)
@@ -1284,11 +1289,334 @@ describe("The user is able to edit 'Language' in the 'Contact data' in the Compa
 
 
         it("Modify the 'Language' field and click on the 'Save' button", function() {
-            cy.get('#MISSING_ID').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#company-language-select').click()
+            cy.wait(1000)
+            cy.xpath('(//*[@role="option"])[1]').click()
             cy.get('#save-button').click()
         });
 
     });
+
+
+
+describe("The user is able to add the 'Sole administrator' in the 'Company settings' section", function() {
+
+        it("On the upper right corner click on the 'User icon' button", function() {
+            cy.wait(5000)
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on 'Company' button", function() {
+            cy.xpath('(//*[@role="menuitem"])[9]').click()
+        });
+
+
+        it("Navigate to the 'None' button and click on it", function() {
+            cy.get('#company-governing-body-select').click()
+        });
+
+        it("From the dropdown menu choose and click on the 'Sole administrator' button and after that populate all required fields", function() {
+            cy.get('#governing-body-1').click()
+            cy.wait(1000)
+            cy.get('#single-admin-name').clear().type(userID_Alpha())
+            cy.get('#single-admin-dni').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#single-admin-email').clear().type('alem'+Cypress.config('UniqueNumber')+'@yopmail.com')
+            cy.get('#single-admin-phone').clear().type(Cypress.config('UniqueNumber'))
+
+
+        });
+
+        it("Click on the “Save” button", function() {
+            cy.get('#save-button').click()
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+
+describe("The user is able to add the 'Boards of directors' in the 'Company Settings' section", function() {
+
+        it("On the upper right corner click on the 'User icon' button", function() {
+            cy.wait(5000)
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on 'Company' button", function() {
+            cy.xpath('(//*[@role="menuitem"])[9]').click()
+        });
+
+
+        it("Navigate to the 'None' button and click on it", function() {
+            cy.get('#company-governing-body-select').click()
+        });
+
+        it("From the dropdown menu choose and click on the 'Boards of directors' button and after that populate all required fields", function() {
+            cy.get('#governing-body-5').click()
+            cy.wait(1000)
+            cy.get('#list-admin-add-button').click()
+            cy.get('#list-admin-name').clear().type(userID_Alpha())
+            cy.get('#list-admin-surname').clear().type(userID_Alpha())
+            cy.get('#list-admin-dni').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#list-admin-email').clear().type('alem'+Cypress.config('UniqueNumber')+'@yopmail.com')
+            cy.get('#list-admin-position').clear().type(userID_Alpha())
+            cy.get('#list-admin-date').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#list-admin-length').clear().type(Cypress.config('UniqueNumber'))
+        });
+
+        it("Click on the “Save” button", function() {
+            cy.get('#save-button').click()
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+describe("The user is able to add the 'Solidarity administrators' in the 'Company Settings' section", function() {
+
+        it("On the upper right corner click on the 'User icon' button", function() {
+            cy.wait(5000)
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on 'Company' button", function() {
+            cy.xpath('(//*[@role="menuitem"])[9]').click()
+        });
+
+
+        it("Navigate to the 'None' button and click on it", function() {
+            cy.get('#company-governing-body-select').click()
+        });
+
+        it("From the dropdown menu choose and click on the 'Solidarity administrators' button and after that populate all required fields", function() {
+            cy.get('#governing-body-4').click()
+            cy.wait(1000)
+            cy.get('#list-admin-add-button').click()
+            cy.get('#list-admin-name').clear().type(userID_Alpha())
+            cy.get('#list-admin-surname').clear().type(userID_Alpha())
+            cy.get('#list-admin-dni').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#list-admin-email').clear().type('alem'+Cypress.config('UniqueNumber')+'@yopmail.com')
+            cy.get('#list-admin-position').clear().type(userID_Alpha())
+            cy.get('#list-admin-date').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#list-admin-length').clear().type(Cypress.config('UniqueNumber'))
+        });
+
+        it("Click on the “Save” button", function() {
+            cy.get('#save-button').click()
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+describe("The user is able to add the 'Joint administrators' in the 'Add company' section", function() {
+
+        it("On the upper right corner click on the 'User icon' button", function() {
+            cy.wait(5000)
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on 'Company' button", function() {
+            cy.xpath('(//*[@role="menuitem"])[9]').click()
+        });
+
+
+        it("Navigate to the 'None' button and click on it", function() {
+            cy.get('#company-governing-body-select').click()
+        });
+
+        it("From the dropdown menu choose and click on the 'Joint administrators' button and after that populate all required fields", function() {
+            cy.get('#governing-body-3').click()
+            cy.wait(1000)
+            cy.get('#list-admin-add-button').click()
+            cy.get('#list-admin-name').clear().type(userID_Alpha())
+            cy.get('#list-admin-surname').clear().type(userID_Alpha())
+            cy.get('#list-admin-dni').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#list-admin-email').clear().type('alem'+Cypress.config('UniqueNumber')+'@yopmail.com')
+            cy.get('#list-admin-position').clear().type(userID_Alpha())
+            cy.get('#list-admin-date').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#list-admin-length').clear().type(Cypress.config('UniqueNumber'))
+        });
+
+        it("Click on the “Save” button", function() {
+            cy.get('#save-button').click()
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+describe("The user is able to add the 'Sole administrator, legal entity' in the 'Add company' section", function() {
+
+        it("On the upper right corner click on the 'User icon' button", function() {
+            cy.wait(5000)
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on 'Company' button", function() {
+            cy.xpath('(//*[@role="menuitem"])[9]').click()
+        });
+
+
+        it("Navigate to the 'None' button and click on it", function() {
+            cy.get('#company-governing-body-select').click()
+        });
+
+        it("From the dropdown menu choose and click on the 'Sole administrator, legal entity' button and after that populate all required fields", function() {
+            cy.get('#governing-body-2').click()
+            cy.wait(1000)
+            cy.get('#entity-admin-entity-name').clear().type(userID_Alpha())
+            cy.get('#entity-admin-name').clear().type(userID_Alpha())
+            cy.get('#entity-admin-dni').clear().type(Cypress.config('UniqueNumber'))
+            cy.get('#entity-admin-email').clear().type('alem'+Cypress.config('UniqueNumber')+'@yopmail.com')
+            cy.get('#entity-admin-phone').clear().type(Cypress.config('UniqueNumber'))
+        });
+
+        it("Click on the “Save” button", function() {
+            cy.get('#save-button').click()
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+describe("The alert message is displayed when the user tries to switch to other type without saving changes", function() {
+
+        it("Click on the 'Council types' button", function() {
+            cy.get('#edit-statutes-block').click()
+            cy.wait(1000)
+        });
+
+        it("Scroll down the page and populate some fields", function() {
+            cy.contains('Email notification of the start of voting').click()
+        });
+
+
+        it("Navigate to the other type of meeting and click on it", function() {
+            cy.contains('Extraordinary General Meeting').click()
+            cy.wait(1000)
+        });
+
+        it("The alert message is successfully displayed", function() {
+            cy.contains('Has unsaved changes')
+        });
+
+        it("Click on the “Save” button", function() {
+            cy.contains('Save').click()
+            cy.wait(1000)
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+
+describe("The alert message is displayed when the user tries to switch to other type without saving changes", function() {
+
+        it("Click on the 'Council types' button", function() {
+            cy.get('#edit-statutes-block').click()
+            cy.wait(1000)
+        });
+
+        it("Scroll down the page and populate some fields", function() {
+            cy.contains('Email notification of the start of voting').click()
+        });
+
+
+        it("Navigate to the other type of meeting and click on it", function() {
+            cy.contains('Extraordinary General Meeting').click()
+            cy.wait(1000)
+        });
+
+        it("The alert message is successfully displayed", function() {
+            cy.contains('Has unsaved changes')
+        });
+
+        it("Click on the “Save” button - MISSING ID", function() {
+            cy.contains('Save').click()
+            cy.wait(1000)
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+describe("The user is able to undo all changes in the 'Council Types' section", function() {
+
+        it("Click on the 'Council types' button", function() {
+            cy.get('#edit-statutes-block').click()
+            cy.wait(1000)
+        });
+
+        it("Click on the “checkbox” in front of some fields", function() {
+            cy.contains('Email notification of the start of voting').click()
+        });
+
+
+        it("Navigate to the other type of meeting and click on it", function() {
+            cy.contains('Extraordinary General Meeting').click()
+            cy.wait(1000)
+        });
+
+        it("Click on the “Undo Changes” button - MISSING ID", function() {
+            cy.get('#MISSING_ID').click()
+            cy.wait(1000)
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+describe("The user is able to edit a type of meeting in the 'Council types' section", function() {
+
+        it("Click on the 'Council types' button", function() {
+            cy.get('#edit-statutes-block').click()
+            cy.wait(1000)
+        });
+
+        it("Click on the “checkbox” in front of some fields", function() {
+            cy.contains('Email notification of the start of voting').click()
+        });
+
+
+        it("Populate fields with new data and click on the 'Save' button", function() {
+            cy.contains('Extraordinary General Meeting').click()
+            cy.wait(1000)
+            cy.get('#MISSING_ID').click()
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+        });
+
+
+    });
+
+
 
 
 
