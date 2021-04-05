0b8ace91a60c293df53432b9f3aa7a07392da9ce
Aaron Fuentes <aaron.fuentes@councilbox.com>
2021-04-05 18:47:18 +0200

    Added selected time style

--- src/components/ovac/notLogged/create/AppointmentDateForm.js
+++ src/components/ovac/notLogged/create/AppointmentDateForm.js
@@ -63,12 +63,13 @@ const AppointmentDateForm = ({ style, appointment, setState }) => {
 				>
 					<Scrollbar>
 						<div style={{ flexGrow: 1 }}>
-							{['10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45', '13:00'].map(date => (
+							{['10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45', '13:00'].map(time => (
 								<DateButton
-									key={date}
-									date={date}
+									key={time}
+									date={time}
+									selected={appointment.time === time}
 									onClick={() => setState({
-										time: date
+										time
 									})}
 									time={appointment.time}
 								/>
@@ -82,26 +83,34 @@ const AppointmentDateForm = ({ style, appointment, setState }) => {
 	);
 };
 
-const DateButton = ({ date, onClick }) => (
-	<div
-		onClick={onClick}
-		style={{
-			border: '2px solid #979797',
-			borderRadius: '5px',
-			width: '100%',
-			height: 'calc(16.6666% - 5px)',
-			padding: '0.6em',
-			fontWeight: '500',
-			cursor: 'pointer',
-			userSelect: 'none',
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center',
-			marginBottom: '5px'
-		}}
-	>
-		{date}
-	</div>
-);
+const DateButton = ({ date, onClick, selected }) => {
+	const primary = getPrimary();
+
+	return (
+		<div
+			onClick={onClick}
+			style={{
+				border: '2px solid #979797',
+				borderRadius: '5px',
+				width: '100%',
+				height: 'calc(16.6666% - 5px)',
+				padding: '0.6em',
+				fontWeight: '500',
+				cursor: 'pointer',
+				userSelect: 'none',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				marginBottom: '5px',
+				...(selected ? {
+					color: 'white',
+					backgroundColor: primary
+				} : {})
+			}}
+		>
+			{date}
+		</div>
+	);
+};
 
 export default withApollo(AppointmentDateForm);
