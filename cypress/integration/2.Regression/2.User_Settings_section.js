94674da66efca3b28d0cad2472630aee15d41c8c
Alem Balic <alem@ballisticecho.com>
2021-05-07 13:59:16 +0200

    -Adding automation for Census, and reworking the code

---
+++ cypress/integration/2.Regression/2.User_Settings_section.js
@@ -0,0 +1,431 @@
+const invalid_emails = ["andrej@qa", "andrej.qa", "andrej@majl.234"];
+const login_url = Cypress.env("baseUrl");
+const valid_password = Cypress.env("login_password");
+const valid_email = Cypress.env("login_email");
+
+beforeEach(function() {
+    cy.restoreLocalStorage();
+});
+
+afterEach(function() {
+    cy.saveLocalStorage();
+});
+
+before(function() {
+    cy.clearLocalStorage();
+    cy.saveLocalStorage();
+});
+
+
+function userID_Alpha() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < 10; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+
+describe("Councilbox login - valid username and password", function() {
+
+     before(function() {
+        
+    });
+
+
+    it("Visits the Councilbox web page", function() {
+        cy.visit(login_url);
+    });
+
+    it("Enters email address", function() {
+        cy.get('input').eq(0)
+            .type('alem@qaengineers.net')    
+            .should("have.value", 'alem@qaengineers.net')
+    });
+
+    it("Enters password", function() {
+        cy.get('input').eq(1)
+            .type('Mostar123!')    
+            .should("have.value", 'Mostar123!')
+    });
+
+    it("Clicks login button", function() {
+        cy.get("#login-button").click();
+        cy.wait(1000)
+    });
+
+});
+
+
+
+
+
+
+
+describe("The user is able to change the 'Name' in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+
+        it("Populate all required fields and click on 'To enter' button", function() {
+            cy.get('#username')
+                .type('alem@qaengineers.net')
+            cy.get('#password')
+                .type('Mostar123!')
+            cy.get('#login-button').click()
+        });
+
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Modify the 'Name' field", function() {
+            cy.get('#user-settings-name')
+                .clear()
+                .type('Balla')
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to change the 'Surname' in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Modify the 'Surname' field", function() {
+            cy.get('#user-settings-surname')
+                .clear()
+                .type('Balic')
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to change the 'Telephone No' in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Modify the 'Telephone No' field", function() {
+            cy.get('#user-settings-phone')
+                .clear()
+                .type(Cypress.config('UniqueNumber'))
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to change the 'Email' in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Modify the 'Email' field", function() {
+            cy.get('#user-settings-email')
+                .clear()
+                .type('test@test.test')
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+        it("Modify the 'Email' field", function() {
+            cy.get('#user-settings-email')
+                .clear()
+                .type('alem@qaengineers.net')
+        });
+
+    });
+
+
+describe("The user is able to select the 'Español' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('English').click()
+            cy.contains('Español').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to select the 'Català' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Español').click()
+            cy.contains('Català').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to select the 'Português' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Català').click()
+            cy.contains('Português').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to select the 'Galego' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Português').click()
+            cy.contains('Galego').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+
+describe("The user is able to select the 'Polsku' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Galego').click()
+            cy.contains('Polsku').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+
+describe("The user is able to select the 'Euskera' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Polsku').click()
+            cy.contains('Euskera').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+
+describe("The user is able to select the 'Français' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Euskera').click()
+            cy.contains('Français').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+    });
+
+describe("The user is able to select the 'English' language in the  'User settings' in the Councilbox", function() {
+    
+        it("Open the browser and enter the URL of the staging environment", function() {
+            cy.visit(login_url);
+            cy.wait(1000);
+        });
+        
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Navigate to the 'Language' section and select the 'English' language and click on the “Save” button", function() {
+            cy.contains('Français').click()
+            cy.contains('English').click()
+        });
+
+        it("User should be able to save changes", function() {
+            cy.get('#user-settings-save-button').click()
+        });
+
+        it("Refresh the Web App", function() {
+            cy.visit(login_url);
+        })
+
+    });
+
+
+describe("The user is able to change password in the Councilbox", function() {
+    
+        it("On the upper right corner click on 'User' icon", function() {
+            cy.get('#user-menu-trigger').click()
+        });
+
+        it("From the drop down menu click on the 'Edit user' button", function() {
+            cy.xpath('//*[@class="fa fa-edit"]').click()
+        });
+
+        it("Click on the 'Change password' button", function() {
+            cy.get('#user-change-password-button').click()
+            cy.wait(1000)
+        });
+
+
+        it("Populate all required fields and click on the 'Save' button", function() {
+            cy.get('#user-current-password')
+                .type('Mostar123!')
+            cy.get('#user-password')
+                .type('Mostar123!')
+            cy.get('#user-password-check')
+                .type('Mostar123!')
+            cy.get('#user-password-save').click()
+        });
+
+    });
---
+++ cypress/integration/TEST/test_specs.js
@@ -0,0 +1,122 @@
+const invalid_emails = ["andrej@qa", "andrej.qa", "andrej@majl.234"];
+const login_url = Cypress.env("baseUrl");
+const valid_password = Cypress.env("login_password");
+const valid_email = Cypress.env("login_email");
+
+beforeEach(function() {
+    cy.restoreLocalStorage();
+});
+
+afterEach(function() {
+    cy.saveLocalStorage();
+});
+
+before(function() {
+    cy.clearLocalStorage();
+    cy.saveLocalStorage();
+});
+
+
+function userID_Alpha() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < 10; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+
+
+
+
+
+
+
+
+
+
+describe("Councilbox login - valid username and password", function() {
+
+     before(function() {
+        
+    });
+
+
+    it("Visits the Councilbox web page", function() {
+        cy.visit(login_url);
+    });
+
+    it("Enters email address", function() {
+        cy.get('input').eq(0)
+            .type('alem@qaengineers.net')    
+            .should("have.value", 'alem@qaengineers.net')
+    });
+
+    it("Enters password", function() {
+        cy.get('input').eq(1)
+            .type('Mostar123!')    
+            .should("have.value", 'Mostar123!')
+    });
+
+    it("Clicks login button", function() {
+        cy.get("#login-button").click();
+        cy.wait(1000)
+    });
+
+});
+
+
+describe("The user is able to delete participant in the 'Manage participants' form in the 'Censuses' section", function() {
+
+        it("From the menu choose and click on the 'Censuses' form", function() {
+            
+            cy.get('#edit-censuses-block').click()
+            cy.wait(1000)
+        });
+
+        it("Set the cursor on already added census and navigate to the 'Manage participants' button and then click on it", function() {
+            cy.get('#census_row_0').trigger('mouseover')
+            cy.wait(1000)
+            cy.get('#census-manage-participants-button').click()
+
+        });
+
+        it("Click on the 'Add participant+'' button", function() {
+            cy.wait(1000)
+            cy.get('#add-census-participant-button').click()
+            cy.wait(1000)
+        });
+
+        it("Populate all required fields", function() {
+            cy.get('#participant-name-input').clear()
+                .type('Test')
+            cy.get('#participant-surname-input').clear()
+                .type('Automation')
+            cy.get('#participant-email-input').clear()
+                .type("test2"+Cypress.config('UniqueNumber')+"@test.test")
+
+        });
+
+        it("Click on the 'Save' button", function() {
+            cy.get('#alert-confirm-button-accept').click()
+            cy.wait(1000)
+        });
+
+        it("Navigate to the already added participant and click on the 'X' button", function() {
+            cy.get('#participant-row-0').trigger('mouseover')
+            cy.get('MISSING_ID').click()
+            cy.wait(1000)
+        });
+
+        it("Back to Home page", function() {
+            cy.visit(login_url);
+            cy.wait(3000)
+        });
+
+
+    });
+
+
+
+
