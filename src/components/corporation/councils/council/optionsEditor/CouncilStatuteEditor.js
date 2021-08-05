7575d3f436e147af689d3645399eeb337fcd4ae9
Aaron Fuentes <aaron.fuentes@councilbox.com>
2021-08-05 18:31:41 +0200

    WIP

--- src/components/corporation/councils/council/optionsEditor/CouncilStatuteEditor.js
+++ src/components/corporation/councils/council/optionsEditor/CouncilStatuteEditor.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 import { MenuItem } from 'material-ui';
 import React from 'react';
 import { withApollo } from 'react-apollo';
-import { Grid, GridItem, Checkbox, SelectInput } from '../../../../../displayComponents';
+import { Grid, GridItem, Checkbox, SelectInput, TextInput } from '../../../../../displayComponents';
 
 
 const CouncilStatuteEditor = ({
@@ -84,8 +84,7 @@ const CouncilStatuteEditor = ({
 					value={statute.hideVotingsRecountFinished === 1}
 					onChange={(event, isInputChecked) => updateState({
 						hideVotingsRecountFinished: isInputChecked ? 1 : 0
-					})
-					}
+					})}
 				/>
 			</GridItem>
 			<GridItem xs={12} md={7} lg={7}>
@@ -94,8 +93,14 @@ const CouncilStatuteEditor = ({
 					value={statute.shareholdersPortal === 1}
 					onChange={(event, isInputChecked) => updateState({
 						shareholdersPortal: isInputChecked ? 1 : 0
-					})
-					}
+					})}
+				/>
+			</GridItem>
+			<GridItem xs={12} md={7} lg={7}>
+				<DecimalDigitsInput
+					updateAction={updateState}
+					value={statute.decimalDigits}
+					translate={translate}
 				/>
 			</GridItem>
 			<GridItem xs={12} md={7} lg={7}>
@@ -191,6 +196,34 @@ const CouncilStatuteEditor = ({
 				</SelectInput>
 			</GridItem>
 		</Grid>
+	);
+};
+
+const DecimalDigitsInput = ({ value, updateAction }) => {
+	const [internalValue, setInternalValue] = React.useState(value);
+
+	React.useState(() => {
+		let timeout;
+
+		if (internalValue !== value) {
+			setTimeout(() => {
+				console.log('se va actualizar');
+				updateAction({ decimalDigits: internalValue });
+			}, 400);
+		}
+
+		return () => clearTimeout(timeout);
+	}, [internalValue]);
+
+	return (
+		<TextInput
+			floatingText={'Número de decimales'}
+			value={internalValue}
+			type="number"
+			onChange={event => setInternalValue({
+				decimalDigits: Number(event.target.value || 0)
+			})}
+		/>
 	);
 };
 
