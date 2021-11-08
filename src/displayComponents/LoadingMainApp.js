44a16bb93ca7bcd52f39bb6d0c9fc34695f4b299
Raul <barranquero.raul@gmail.com>
2021-11-08 19:33:26 +0100

    Add notificationsBranding as variable and adjust mobil display

--- src/displayComponents/LoadingMainApp.js
+++ src/displayComponents/LoadingMainApp.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ConfigContext } from '../containers/AppControl';
+import { isMobile } from '../utils/screen';
 import tips from '../assets/img/tips.png';
 import title from '../assets/img/logo-xs.png';
-import withSharedProps from '../HOCs/withSharedProps';
+import withTranslations from '../HOCs/withTranslations';
 
 const LoadingMainApp = ({
-	error, message, translate, company, displayPopUp
+	error, message, translate, company, displayAdvice
 }) => {
 	if (error) {
 		window.location.reload();
 	}
 
-	if (displayPopUp) {
+	const config = useContext(ConfigContext);
+
+	if (displayAdvice) {
 		return (
 			<div
 				style={{
@@ -28,11 +32,13 @@ const LoadingMainApp = ({
 					justifyContent: 'center',
 					flexDirection: 'column'
 				}}>
-					<img src={company.logo} alt="councilbox logo" style={{ maxHeight: '60px' }} />
-					<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+					{config.notificationsBranding && <img src={company.logo} alt="councilbox logo" style={{ maxHeight: isMobile ? '80px' : '60px', paddingTop: isMobile ? '40px' : '0' }} /> }
+					<div style={{
+						display: 'flex', flexDirection: isMobile ? 'column-reverse' : 'row', alignItems: 'center', paddingBottom: isMobile ? '40px' : null, textAlign: isMobile ? 'center' : null
+					}}>
 						<img src="/img/logo-icono.png" className="element-animation" alt="councilbox logo" style={{ width: '30px', maxHeight: '30px' }}/>
 						<p style={{
-							fontSize: '22px', padding: '45px 25px 25px', color: '#7BA1B1'
+							fontSize: isMobile ? '17px' : '22px', padding: isMobile ? '35px 35px 25px 40px' : '45px 25px 25px', color: '#7BA1B1'
 						}}>{translate.opening_room_dont_close_navigator}</p>
 					</div>
 					<div>
@@ -44,12 +50,14 @@ const LoadingMainApp = ({
 							display: 'flex',
 							justifyContent: 'spread-between',
 						}}>
-							<img src={tips} alt="tips logo" style={{ padding: '25px 25px 35px 20px', verticalAlign: 'top' }}/>
-							<div style={{ padding: '20px 0' }}>
+							{!isMobile && <img src={tips} alt="tips logo" style={{
+								padding: '25px 25px 35px 20px', verticalAlign: 'top'
+							}}/> }
+							<div style={{ padding: isMobile ? '25px' : '20px 0' }}>
 								<div style={{ display: 'flex', flexDirection: 'row' }}>
 									<p style={{ fontSize: '20px', color: '#7BA1B1', marginBottom: '7px' }}>{translate.advices}</p>
-									<span style={{ padding: '2px 10px' }}>
-										<img src={title} style={{ width: '60%' }}/>
+									<span style={{ padding: isMobile ? '4px 10px' : '2px 10px' }}>
+										<img src={title} style={{ width: isMobile ? '3.5rem' : '60%' }}/>
 									</span>
 								</div>
 								<p style ={{ fontSize: '14px' }} dangerouslySetInnerHTML={{ __html: translate.loading_screen_marketing_text }} />
@@ -80,5 +88,5 @@ const LoadingMainApp = ({
 };
 
 
-export default withSharedProps()(LoadingMainApp);
+export default withTranslations()(LoadingMainApp);
 
