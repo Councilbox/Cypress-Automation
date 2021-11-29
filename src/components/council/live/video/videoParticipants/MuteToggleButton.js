35287ea14a2be95d4294d288c0e89b456a4da335
Aaron Fuentes <aaron.fuentes@councilbox.com>
2021-11-29 18:12:46 +0100

    CREU-411

--- src/components/council/live/video/videoParticipants/MuteToggleButton.js
+++ src/components/council/live/video/videoParticipants/MuteToggleButton.js
@@ -6,68 +6,43 @@ import { flowRight as compose } from 'lodash';
 import { getSecondary } from '../../../../../styles/colors';
 import { haveGrantedWord } from '../../../../../utils/CBX';
 
-class MuteToggleButton extends React.Component {
-toggleMuteParticipant = async () => {
-	this.setState({
-		loading: true
-	});
-	if ((this.props.participant.videoParticipant && this.props.participant.videoParticipant.mutedMic)) {
-		const response = await this.props.unmuteParticipant({
-			variables: {
-				councilId: this.props.participant.councilId,
-				videoParticipantId: this.props.participant.videoParticipant.id
-			}
-		});
+const MuteToggleButton = ({ translate, participant, refetch, muteParticipant }) => {
+	const disabled = participant.videoParticipant && participant.videoParticipant.mutedMic;
 
-		if (response.data) {
-			if (response.data.unmuteVideoParticipant.success) {
-				this.setState({
-					loading: false
-				});
-				this.props.refetch();
+	const toggleMuteParticipant = async () => {
+		if ((participant.videoParticipant && !participant.videoParticipant.mutedMic)) {
+			const response = await muteParticipant({
+				variables: {
+					councilId: participant.councilId,
+					videoParticipantId: participant.videoParticipant.id
+				}
+			});
+
+			if (response.data) {
+				if (response.data.muteVideoParticipant.success) {
+					refetch();
+				}
 			}
 		}
-	} else {
-		const response = await this.props.muteParticipant({
-			variables: {
-				councilId: this.props.participant.councilId,
-				videoParticipantId: this.props.participant.videoParticipant.id
-			}
-		});
+	};
 
-		if (response.data) {
-			if (response.data.muteVideoParticipant.success) {
-				this.setState({
-					loading: false
-				});
-				this.props.refetch();
-			}
-		}
-	}
-}
-
-render() {
-	const { participant } = this.props;
-
-	// TRADUCCION
 	return (
 		<div style={{ marginRight: '0.3em' }}>
 			{haveGrantedWord(participant) && (
 				<Tooltip
 					title={
-						participant.requestWord === 2 ?
-							'Mutar participante'
-							: ''
+						!disabled ?
+							translate.mute_microphone
+							: translate.only_participant_can_enable_audio
 					}
 				>
 					<Card
-						onClick={() => this.toggleMuteParticipant(participant.id)
-						}
+						onClick={disabled ? () => {} : () => toggleMuteParticipant(participant.id)}
 						style={{
 							width: '1.6em',
 							height: '1.6em',
 							borderRadius: '0.1em',
-							backgroundColor: getSecondary()
+							backgroundColor: disabled ? 'grey' : getSecondary()
 						}}
 					>
 						<MenuItem
@@ -82,7 +57,7 @@ render() {
 								justifyContent: 'center'
 							}}
 						>
-							{(this.props.participant.videoParticipant && this.props.participant.videoParticipant.mutedMic) ?
+							{(participant.videoParticipant && participant.videoParticipant.mutedMic) ?
 								<i className="fa fa-microphone-slash" aria-hidden="true" style={{ transform: 'scaleX(-1)' }}></i>
 								: <i className="fa fa-microphone" aria-hidden="true"></i>
 							}
@@ -92,8 +67,7 @@ render() {
 			)}
 		</div>
 	);
-}
-}
+};
 
 const muteParticipant = gql`
 	mutation muteParticipant($videoParticipantId: String!, $councilId: Int!){
