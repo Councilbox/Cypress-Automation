8091f7d99f6713591006f2a27a9a4098299882a4
Muhamed Adzamija <muhamedadzamija@Muhameds-MacBook-Air.local>
2022-09-23 12:08:54 +0200

    update

--- cypress/integration/1.Smoke/1.Account_section.js
+++ cypress/integration/1.Smoke/1.Account_section.js
@@ -5,73 +5,61 @@ const login_url = Cypress.env("baseUrl");
 const valid_password = Cypress.env("login_password");
 const valid_email = Cypress.env("login_email");
 
+let login = new loginPage();
 
-let login = new loginPage()
+describe("Account section", function() {
+	before(function() {});
 
-describe("The user is able to create a new account in Councilbox", function() {
+	it("The user is able to create a new account in Councilbox", function() {
+		const name = "Automation";
+		const surname = "Test";
+		const phone = "123123123";
+		const email = "alem" + Cypress.config("UniqueNumber") + "@yopmail.com";
+		const password = "Test12345";
 
-    before(function() {
-      
-    });
-    
-    it("Open the browser and enter the URL: https://app.councilbox.com/", function() {       
-        cy.visit(login_url);
-        cy.wait(5000);
-    });
+		cy.clearLocalStorage();
 
-    it("Change language to Spanish", function() {
-        login.click_on_language_dropmenu()
-        cy.get('#language-es').click();
-    });
+		cy.log(
+			"Open the browser and enter the URL: https://app.councilbox.com/"
+		);
+		cy.visit(login_url);
+		cy.wait(5000);
 
-    it("Click on the 'Registarse' button", function() {
-        cy.get("#sign-up-button").click();
-    });
+		cy.log("Change language to Spanish");
+		login.click_on_language_dropmenu();
+		login.select_spanish_language();
 
-    it("Populate all required fields", function() {
-        cy.get('#signup-name').clear()
-            .type("Automation")
-            .should("have.value", "Automation")
-        cy.get('#signup-surname').clear()
-            .type("Test")
-            .should("have.value", "Test")
-        cy.get('#signup-phone').clear()
-            .type("123123123")
-            .should("have.value", "123123123")
-        cy.get('#signup-email').clear()
-            .type("alem"+Cypress.config('UniqueNumber')+"@yopmail.com")            
-        cy.get('#signup-email-check').clear()
-            .type("alem"+Cypress.config('UniqueNumber')+"@yopmail.com")
-        cy.get('#signup-password').clear()
-            .type("Test12345")
-        cy.get('#signup-password-check').clear()
-            .type("Test12345")    
-    });
+		cy.log("Click on the 'Registarse' button");
+		cy.get("#sign-up-button").click();
 
-    it("Click on the checkbox button to accept the 'términos y condiciones de councilbox'", function() {
-        cy.get('#accept-legal-checkbox').click();
-    });
+		cy.log("Populate all required fields");
+		login.enter_signup_name(name);
+		login.enter_signup_surname(surname);
+		login.enter_signup_phone(phone);
+		login.enter_signup_email_and_confirm_it(email);
+		login.enter_signup_password(password);
+		login.enter_signup_password_confirm(password);
 
-    it("Click on the 'Enviar button'", function() {
-        cy.get('#create-user-button').click();
-    });
+		cy.log(
+			"Click on the checkbox button to accept the 'términos y condiciones de councilbox'"
+		);
+		cy.get("#accept-legal-checkbox").click();
 
-    it("User should be registered successfully", function() {
-        cy.xpath('//*[@id="root"]/div[1]/div/div/div[1]/div/div[2]/div/div[1]/div[2]/div')
-            .should('be.visible')
-        cy.wait(1000)
-    });
+		cy.log("Click on the 'Enviar button'");
+		cy.get("#create-user-button").click();
 
-    it("Open email application and navigate to the activation email", function() {
-        
-       /*
+		cy.log("User should be registered successfully");
+		cy.get("#signup-back-button").should("be.visible");
+
+		cy.log("Open email application and navigate to the activation email");
+		/*
        cy.visit('http://www.yopmail.com/en/')    
        cy.get('#login').clear()
        		.type("alem"+Cypress.config('UniqueNumber'))
        cy.get('.sbut').click()   
-       */    
-   });
-/*
+       */
+	});
+	/*
     it('GET', function(){
 
         cy.request({
@@ -102,80 +90,69 @@ describe("The user is able to create a new account in Councilbox", function() {
 }
 
 */
-    
-
-
 });
-
-
-
 
 describe("Councilbox login - valid username and password", function() {
+	it("Visits the Councilbox web page", function() {
+		cy.clearLocalStorage();
+		cy.saveLocalStorage();
+		cy.visit(login_url);
+	});
 
-    it("Visits the Councilbox web page", function() {
-        cy.clearLocalStorage();
-        cy.saveLocalStorage();
-        cy.visit(login_url);
-    });
+	it("Change language to Spanish", function() {
+		cy.get("#language-selector").click();
+		cy.get("#language-es").click();
+	});
 
-    it("Change language to Spanish", function() {
-        cy.get('#language-selector').click();
-        cy.get('#language-es').click();
-    });
+	it("Enters email address", function() {
+		cy.get("#username")
+			.clear()
+			.type("alem@qaengineers.net")
+			.should("have.value", "alem@qaengineers.net");
+	});
 
-    it("Enters email address", function() {
-        cy.get('#username').clear()
-            .type('alem@qaengineers.net')    
-            .should("have.value", 'alem@qaengineers.net')
-    });
+	it("Enters password", function() {
+		cy.get("#password")
+			.clear()
+			.type("Mostar123!test")
+			.should("have.value", "Mostar123!test");
+	});
 
-    it("Enters password", function() {
-        cy.get('#password').clear()
-            .type('Mostar123!test')    
-            .should("have.value", 'Mostar123!test')
-    });
-
-    it("Clicks login button", function() {
-        cy.get("#login-button").click();
-    });
-
+	it("Clicks login button", function() {
+		cy.get("#login-button").click();
+	});
 });
-
-
 
 describe("Log Out", function() {
+	it("Opens dropdown in upper right corner", function() {
+		cy.get("#user-menu-trigger").click();
+	});
+	it("Clicks logout", function() {
+		cy.get("#user-menu-logout").click();
+	});
 
-    it("Opens dropdown in upper right corner", function() {
-        cy.get('#user-menu-trigger').click()
-    });
-    it("Clicks logout", function() {
-        cy.get('#user-menu-logout').click()
-    });
-
-    it("User successfully redirected to login page", function() {
-        cy.url().should("include", login_url);
-    });
+	it("User successfully redirected to login page", function() {
+		cy.url().should("include", login_url);
+	});
 });
-
 
 describe("The user is able to restore a password", function() {
+	it("Click on the 'Olvide mi contrasena?'' button", function() {
+		cy.get("#restore-password-link").click();
+	});
 
-    it("Click on the 'Olvide mi contrasena?'' button", function() {
-        cy.get('#restore-password-link').click()
-    });
+	it("Enter your Email and click on the 'Restablecer acceso' button", function() {
+		cy.get("#restore-password-email-input")
+			.clear()
+			.type("alemtestqa12@yopmail.com");
+		cy.get("#restore-password-button").click();
+	});
 
-    it("Enter your Email and click on the 'Restablecer acceso' button", function() {
-        cy.get('#restore-password-email-input').clear()
-        	.type('alemtestqa12@yopmail.com')
-        cy.get('#restore-password-button').click()
-    });
+	it("Open your email application", function() {
+		cy.visit("http://www.yopmail.com/en/");
+	});
 
-    it("Open your email application", function() {
-       cy.visit('http://www.yopmail.com/en/')
-
-});
-
-    /*
+	/*
      it("Open 'Restablecer contrasena' email and click on the 'Restablecer contrasena' button", function() {
        cy.get('#login').type('alemtestqa12')
        cy.get('.sbut').click()      
@@ -208,8 +185,7 @@ describe("The user is able to restore a password", function() {
 });
 
 */
-it("Back to Home page", function() {
-            cy.visit(login_url);
-        });    
+	it("Back to Home page", function() {
+		cy.visit(login_url);
+	});
 });
-
